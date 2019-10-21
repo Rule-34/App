4e48944cdcc208a44c4242d51fe5fd746978e53e
Alejandro Akbal <37181533+VoidlessSeven7@users.noreply.github.com>
2019-10-21 19:11:34 +0200

    Basic skeleton added+

---
+++ src/app.js
@@ -0,0 +1,9 @@
+import Vue from 'vue';
+
+// Import components
+import App from './js/components/Example';
+
+new Vue({
+    el: '#app',
+    render: h => h(App),
+});
---
+++ src/js/components/Example.vue
@@ -0,0 +1,31 @@
+
+<template>
+  <h1>{{name}}</h1>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  data: function() {
+    return {
+      name: "Hello World!!!!"
+    };
+  }
+});
+</script>
+
+<style>
+h1 {
+  color: white;
+  background-color: black;
+}
+</style>
+<style lang="scss">
+$primary-color: white;
+$bg: black;
+h1 {
+  color: $primary-color;
+  background-color: $bg;
+}
+</style>
