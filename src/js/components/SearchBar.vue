3cef77c53a6665abb522cb98deccabff0c281066
Alejandro Akbal <37181533+VoidlessSeven7@users.noreply.github.com>
2019-10-26 13:19:49 +0200

    Added: VueX
    Getting modularity all around

---
+++ src/js/components/SearchBar.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="card-container md:w-4/5"></div>
+</template>
+
+<script>
+export default {
+  name: "search-bar",
+  // props: ["category"],
+  data() {
+    return {};
+  },
+  methods: {}
+};
+</script>
+
