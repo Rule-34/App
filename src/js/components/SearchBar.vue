df368b7291e922e9301cf095cf73a15e31e008b6
Alejandro Akbal <37181533+VoidlessSeven7@users.noreply.github.com>
2019-10-30 15:56:28 +0100

    Littler icons, better search grid

--- src/js/components/SearchBar.vue
+++ src/js/components/SearchBar.vue
@@ -6,17 +6,17 @@
     <!-- Centered container -->
     <div class="flex flex-wrap md:flex-no-wrap h-screen">
       <!-- Separator -->
-      <div class="hidden md:block w-2/6" @click.self="toggleSearch"></div>
+      <div class="hidden md:block w-1/6" @click.self="toggleSearch"></div>
       <!-- Search bar -->
       <div class="w-full md:w-2/6 m-auto">
         <div class="post-container p-2 inline-flex w-3/4">
           <!-- Search Icon -->
-          <search-icon class="pointer-events-none text-grey-darkest w-6 h-6 mr-2"></search-icon>
+          <search-icon class="icon text-black w-6 h-6 mr-2"></search-icon>
           <!-- Input -->
           <input
-            class="w-full outline-none"
+            class="w-full ml-1 outline-none font-light"
             type="search"
-            placeholder="Search: e.g. dragons"
+            placeholder="Search: e.g. dragon"
             autofocus
             v-model="searchQuery"
             v-debounce:300ms="getTags"
@@ -25,15 +25,29 @@
       </div>
 
       <!-- Results -->
-      <div class="post-container w-full md:w-2/6 p-2 flex flex-col">
+      <div class="post-container max-h-3/4 min-h-1/2 w-full md:w-2/6 flex flex-col p-2 my-auto">
+        <!-- Show if theres nothing -->
+        <h1
+          class="text-center font-hairline m-auto text-xl"
+          v-if="!searchData.data"
+        >Search something!</h1>
+
         <!-- Tags -->
-        <div v-for="tag in searchData.data" :key="tag.name">
-          <button v-text="tag.name"></button>
+        <div class="tag-container" v-if="searchData.data">
+          <a class="tag group" v-for="tag in searchData.data" :key="tag.name">
+            {{tag.name}}
+            <span
+              class="text-gray-400 group-hover:text-gray-600"
+              v-text="'(' + tag.posts + ')'"
+            ></span>
+          </a>
         </div>
 
         <!-- Apply tags -->
-        <button class="btn text-white bg-gradient-lilac-blue mt-auto">Apply tags</button>
+        <button class="btn text-white bg-gradient-lilac-blue mt-auto shadow-md">Apply tags</button>
       </div>
+
+      <div class="hidden md:block w-1/6" @click.self="toggleSearch"></div>
       <!--  -->
     </div>
   </div>
