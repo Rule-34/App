7d3a1d824484b17cb92dbfd2da7bac5985997887
Alejandro Akbal <37181533+VoidlessSeven7@users.noreply.github.com>
2019-11-01 12:52:34 +0100

    Fixed specific page ID, better tags

--- components/Post.vue
+++ components/Post.vue
@@ -15,7 +15,8 @@
     <!-- Video -->
     <lazy-component v-if="post.type == 'video'" @click="toggleTags">
       <video class="post-img" :alt="post.type" controls autoplay muted loop>
-        <source :src="post.file_url" />
+        <!-- prettier-ignore -->
+        <source :src="post.file_url">
         Your browser doesnt support HTML5 video.
       </video>
     </lazy-component>
@@ -35,17 +36,24 @@
     <!-- Tags and source -->
     <div
       v-if="post.source || (post.tags && isActive)"
-      class="flex flex-wrap overflow-hidden text-sm"
+      class="flex flex-wrap overflow-hidden text-sm p-1"
     >
       <!-- Tags -->
-      <div v-if="post.tags && isActive" class="w-full md:w-11/12 tag-container">
-        <a v-for="tag in post.tags" :key="post[tag]" class="tag" v-text="tag" />
+      <div
+        v-if="post.tags && isActive"
+        class="post-extra w-full md:w-11/12 tag-container"
+        :class="{ 'md:w-full': !post.source }"
+      >
+        <a
+          v-for="tag in post.tags"
+          :key="post[tag]"
+          class="tag"
+          href="javascript:void(0)"
+          v-text="tag"
+        />
       </div>
       <!-- Source -->
-      <div
-        v-if="post.source"
-        class="w-full md:w-1/12 md:m-auto mt-3 -m-1 text-center"
-      >
+      <div v-if="post.source" class="w-full md:w-1/12 md:m-auto text-center">
         <a
           class="inline-flex items-baseline"
           :href="post.source"
