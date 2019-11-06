7a868cfc698167ba72133c1ab078f478374fd097
Alejandro Akbal <37181533+VoidlessSeven7@users.noreply.github.com>
2019-11-06 15:19:17 +0100

    Removing unnecessary packages

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,24 +1,23 @@
+
 export default {
   mode: 'spa',
   /*
-   ** Headers of the page
-   */
+  ** Headers of the page
+  */
   head: {
-    title: 'Material Rule 34',
+    title: 'Rule 34 Progressive Web App',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      {
-        hid: 'description',
-        name: 'description',
-        content: process.env.npm_package_description || ''
-      }
+      { hid: 'description', name: 'description', content: 'Rule 34 redesigned' }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
   },
   /*
-   ** Customize the progress-bar color
-   */
+  ** Customize the progress-bar color
+  */
   loading: {
     css: false,
     color: 'white',
@@ -34,12 +33,13 @@ export default {
     background: 'linear-gradient(152deg, #9b9be0 38%, #00d4ff 100%)'
   },
   /*
-   ** Global CSS
-   */
-  // css: [],
+  ** Global CSS
+  */
+  css: [
+  ],
   /*
-   ** Plugins to load before mounting the App
-   */
+  ** Plugins to load before mounting the App
+  */
   plugins: [
     {
       src: '~/plugins/v-debounce.js',
@@ -49,31 +49,34 @@ export default {
     { src: '~/plugins/insights.js', mode: 'client' }
   ],
   /*
-   ** Nuxt.js dev-modules
-   */
+  ** Nuxt.js dev-modules
+  */
   buildModules: [
-    // Doc: https://github.com/nuxt-community/eslint-module
-    '@nuxtjs/eslint-module',
     // Doc: https://github.com/nuxt-community/nuxt-tailwindcss
-    '@nuxtjs/tailwindcss'
+    '@nuxtjs/tailwindcss',
   ],
+  /*
+  ** TailwindCSS settings
+  */
   tailwindcss: {
     configPath: '~tailwind.config.js',
     cssPath: '~/assets/app.scss'
   },
   /*
-   ** Nuxt.js modules
-   */
+  ** Nuxt.js modules
+  */
   modules: [
     // Doc: https://axios.nuxtjs.org/usage
     '@nuxtjs/axios',
     '@nuxtjs/pwa',
-    'nuxt-purgecss'
+	'nuxt-purgecss',
   ],
-
+  /*
+  ** Progressive web app
+  */
   pwa: {
     manifest: {
-      name: 'Material Rule 34',
+      name: 'Rule 34 Progressive Web App',
       lang: 'en'
     },
     meta: {
@@ -81,25 +84,28 @@ export default {
       ogHost: 'https://r34.app'
     }
   },
-
+  /*
+  ** PurgeCSS config
+  */
   purgeCSS: {
     // whitelist: ['defaults-and-this-class']
     whitelistPatterns: [/active/]
   },
   /*
-   ** Axios module configuration
-   ** See https://axios.nuxtjs.org/options
-   */
-  axios: {},
+  ** Axios module configuration
+  ** See https://axios.nuxtjs.org/options
+  */
+  axios: {
+  },
   /*
-   ** Build configuration
-   */
+  ** Build configuration
+  */
   build: {
-    extractCSS: true,
+	extractCSS: true,
     /*
-     ** You can extend webpack config here
-     */
-    // eslint-disable-next-line no-unused-vars
-    extend(config, ctx) {}
+    ** You can extend webpack config here
+    */
+    extend (config, ctx) {
+    }
   }
 }
