f087a148cebe5b6d231105d9f260bacbbd02b245
Alejandro Akbal <37181533+VoidlessSeven7@users.noreply.github.com>
2019-11-06 15:26:20 +0100

    Changing name since it doesnt have material at all

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,111 +1,105 @@
-
 export default {
-  mode: 'spa',
+  mode: "spa",
   /*
-  ** Headers of the page
-  */
+   ** Headers of the page
+   */
   head: {
-    title: 'Rule 34 Progressive Web App',
+    title: "Rule 34 PWA",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Rule 34 redesigned' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "Rule 34 redesigned" }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
   },
   /*
-  ** Customize the progress-bar color
-  */
+   ** Customize the progress-bar color
+   */
   loading: {
     css: false,
-    color: 'white',
-    failedColor: 'red',
-    height: '10px'
+    color: "white",
+    failedColor: "red",
+    height: "10px"
   },
   /*
    ** Customize the splash loading indicator
    */
   loadingIndicator: {
-    name: 'cube-grid',
-    color: 'white',
-    background: 'linear-gradient(152deg, #9b9be0 38%, #00d4ff 100%)'
+    name: "cube-grid",
+    color: "white",
+    background: "linear-gradient(152deg, #9b9be0 38%, #00d4ff 100%)"
   },
   /*
-  ** Global CSS
-  */
-  css: [
-  ],
+   ** Global CSS
+   */
+  css: [],
   /*
-  ** Plugins to load before mounting the App
-  */
+   ** Plugins to load before mounting the App
+   */
   plugins: [
     {
-      src: '~/plugins/v-debounce.js',
-      mode: 'client'
+      src: "~/plugins/v-debounce.js",
+      mode: "client"
     },
-    { src: '~/plugins/v-lazy.js', mode: 'client' },
-    { src: '~/plugins/insights.js', mode: 'client' }
+    { src: "~/plugins/v-lazy.js", mode: "client" },
+    { src: "~/plugins/insights.js", mode: "client" }
   ],
   /*
-  ** Nuxt.js dev-modules
-  */
+   ** Nuxt.js dev-modules
+   */
   buildModules: [
     // Doc: https://github.com/nuxt-community/nuxt-tailwindcss
-    '@nuxtjs/tailwindcss',
+    "@nuxtjs/tailwindcss"
   ],
   /*
-  ** TailwindCSS settings
-  */
+   ** TailwindCSS settings
+   */
   tailwindcss: {
-    configPath: '~tailwind.config.js',
-    cssPath: '~/assets/app.scss'
+    configPath: "~tailwind.config.js",
+    cssPath: "~/assets/app.scss"
   },
   /*
-  ** Nuxt.js modules
-  */
+   ** Nuxt.js modules
+   */
   modules: [
     // Doc: https://axios.nuxtjs.org/usage
-    '@nuxtjs/axios',
-    '@nuxtjs/pwa',
-	'nuxt-purgecss',
+    "@nuxtjs/axios",
+    "@nuxtjs/pwa",
+    "nuxt-purgecss"
   ],
   /*
-  ** Progressive web app
-  */
+   ** Progressive web app
+   */
   pwa: {
     manifest: {
-      name: 'Rule 34 Progressive Web App',
-      lang: 'en'
+      name: "Rule 34 PWA",
+      lang: "en"
     },
     meta: {
       /* meta options */
-      ogHost: 'https://r34.app'
+      ogHost: "https://r34.app"
     }
   },
   /*
-  ** PurgeCSS config
-  */
+   ** PurgeCSS config
+   */
   purgeCSS: {
     // whitelist: ['defaults-and-this-class']
     whitelistPatterns: [/active/]
   },
   /*
-  ** Axios module configuration
-  ** See https://axios.nuxtjs.org/options
-  */
-  axios: {
-  },
+   ** Axios module configuration
+   ** See https://axios.nuxtjs.org/options
+   */
+  axios: {},
   /*
-  ** Build configuration
-  */
+   ** Build configuration
+   */
   build: {
-	extractCSS: true,
+    extractCSS: true,
     /*
-    ** You can extend webpack config here
-    */
-    extend (config, ctx) {
-    }
+     ** You can extend webpack config here
+     */
+    extend(config, ctx) {}
   }
-}
+};
