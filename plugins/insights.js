86227e90d76ed453ab3337f610341e21427fb660
Alejandro Akbal <37181533+VoidlessSeven7@users.noreply.github.com>
2019-11-07 11:43:54 +0100

    Better analytics

--- plugins/insights.js
+++ plugins/insights.js
@@ -1,18 +1,9 @@
-import { init, track, parameters } from 'insights-js'
+import { init, trackPages } from "insights-js";
 
-const debug = process.env.NODE_ENV !== 'production'
+const debug = process.env.NODE_ENV !== "production";
 
 /* -------- Analytics -------- */
 if (!debug) {
-  init('kQrGvntXWy9eDO4h')
-  track({
-    id: 'user-usage',
-    parameters: {
-      path: parameters.path(),
-      locale: parameters.locale(),
-      screenType: parameters.screenType(),
-      referrer: parameters.referrer(),
-      duration: parameters.durationInterval(5000)
-    }
-  })
+  init("kQrGvntXWy9eDO4h");
+  trackPages();
 }
