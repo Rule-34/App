e78dee3242deca115c4cd7fbd0e16424ef8c3457
Alejandro Akbal <37181533+VoidlessSeven7@users.noreply.github.com>
2019-11-07 15:59:01 +0100

    Fixed optimum height, added Akbal as static image, finalized settings ui

--- pages/about.vue
+++ pages/about.vue
@@ -4,18 +4,14 @@
     <div class="cool-bar" />
     <SideNav :show-search="false" />
     <div class="card-container md:w-2/3 xl:w-1/2">
-      <div class="flex h-screen p-3">
+      <div class="flex h-perfect p-3">
         <div class="post-container m-auto p-5 shadow-xl">
           <a
             href="https://akbal.dev/"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <img
-              class="mx-auto mb-5"
-              src="https://akbal.dev/images/ALogo.png"
-              alt="Akbal logo"
-            />
+            <img class="mx-auto mb-5" src="/img/ALogo.png" alt="Akbal logo" />
           </a>
           <div class="text-center">
             <h1 class="btn">Alejandro Akbal</h1>
--- pages/settings.vue
+++ pages/settings.vue
@@ -4,15 +4,27 @@
     <div class="cool-bar" />
     <SideNav :show-search="false" />
     <div class="card-container md:w-2/3 xl:w-1/2">
-      <h1>Settings</h1>
-      <div>
-        <SettingSwitch
-          v-for="(setting, index) in userSettings"
-          :key="setting.name"
-          :value="setting.value"
-          :text="setting.name"
-          :switch-id="index"
-        />
+      <div class="flex h-perfect p-3">
+        <div
+          class="post-container w-full md:w-2/3 xl:w-1/2 m-auto p-5 shadow-xl"
+        >
+          <div class="flex flex-wrap">
+            <div class="w-2/5 m-auto">
+              <h1 class="text-center text-lg">Settings</h1>
+            </div>
+            <div class="w-3/5 m-auto flex">
+              <div class="mx-auto">
+                <SettingSwitch
+                  v-for="(setting, index) in userSettings"
+                  :key="setting.name"
+                  :value="setting.value"
+                  :text="setting.name"
+                  :switch-id="index"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
