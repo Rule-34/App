bc94ec3676cf62e5e16273b15aba71854062e049
Alejandro Akbal <37181533+VoidlessSeven7@users.noreply.github.com>
2019-11-07 12:16:51 +0100

    Fixed specific page loading

--- store/mutations.js
+++ store/mutations.js
@@ -1,29 +1,29 @@
 export default {
   // Handler for post's data changes
   newDashBoardData(state, payload) {
-    // console.log(payload.data)
+    // console.log(payload);
     // Data
     if (payload.data !== undefined) {
       // console.log(payload.data)
-      state.dashBoardData.data = payload.data
+      state.dashBoardData.data = payload.data;
     }
 
     // Errors
     if (payload.errors !== undefined) {
-      state.generalData.errors = payload.errors
+      state.generalData.errors = payload.errors;
     }
 
     // Page ID
     if (payload.pid !== undefined) {
       // console.log(payload.pid);
-      state.dashBoardData.pid = payload.pid
+      state.dashBoardData.pid = payload.pid;
     }
   },
 
   // Handler for api changes
   newApiUrl(state, payload) {
     // New url
-    state.generalData.apiUrl = payload.newUrl
+    state.generalData.apiUrl = payload.newUrl;
   },
 
   // Handler for Search changes
@@ -31,33 +31,33 @@ export default {
   newSearchData(state, payload) {
     // Apply "active" css class
     if (payload.isActive !== undefined) {
-      state.searchData.isActive = payload.isActive
+      state.searchData.isActive = payload.isActive;
     }
 
     // Errors
     if (payload.errors !== undefined) {
-      state.generalData.errors = payload.errors
+      state.generalData.errors = payload.errors;
     }
     // Data
     if (payload.data !== undefined) {
       // console.log(payload.data);
-      state.searchData.data = payload.data
+      state.searchData.data = payload.data;
     }
 
     // Added tags
     if (payload.tag !== undefined) {
       // console.log(payload.tag.function);
 
-      if (payload.tag.function === 'add') {
+      if (payload.tag.function === "add") {
         // console.log(payload.tag.name);
-        state.searchData.tags.push(payload.tag.name)
+        state.searchData.tags.push(payload.tag.name);
       }
-      if (payload.tag.function === 'remove') {
+      if (payload.tag.function === "remove") {
         // console.log(payload.tag.name);
         state.searchData.tags = state.searchData.tags.filter(function(ele) {
-          return ele !== payload.tag.name
-        })
+          return ele !== payload.tag.name;
+        });
       }
     }
   }
-}
+};
