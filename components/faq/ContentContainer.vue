f19ebaa174dfac8ba9ef874c35f4a66010bae2e7
Alejandro Akbal <37181533+VoidlessSeven7@users.noreply.github.com>
2019-11-11 15:59:25 +0100

    Improved usage page and ContentContainer

--- components/faq/ContentContainer.vue
+++ components/faq/ContentContainer.vue
@@ -1,49 +1,65 @@
 <template>
   <!-- Loop for every info container -->
   <div class="card-container" :class="{ zoom: userSettings.zoom.value }">
-    <div class="material-container p-3">
-      <!-- Head -->
-      <div class="flex inline-flex align-middle mb-2">
-        <InfoIcon v-if="icon === 'info'" class="mr-2 text-blue-500" />
-        <StarIcon v-else-if="icon === 'star'" class="mr-2 text-yellow-500" />
-        <DollarSignIcon
-          v-else-if="icon === 'donation'"
-          class="mr-2 text-green-500"
-        />
-        <h1 class="text-lg" v-text="title"></h1>
+    <!-- If separator -->
+    <template v-if="separator">
+      <div
+        class="material-container w-fit-content mx-auto p-3 text-center bg-gradient-blue-lilac text-white"
+      >
+        <!-- Head -->
+        <div class="mb-2">
+          <h1 class="text-lg underline" v-text="title"></h1>
+        </div>
+
+        <!-- Body -->
+        <div v-if="text" class="text-sm">
+          <p class="mb-1" v-text="text"></p>
+          <!-- Slot for extra info -->
+        </div>
       </div>
+    </template>
 
-      <!-- Body -->
-      <div class="text-sm">
-        <p
-          v-if="text"
-          class="mb-1"
-          :class="{ 'whitespace-pre-line': text }"
-          v-text="text"
-        ></p>
+    <!-- If normal post -->
+    <template v-else>
+      <div class="material-container p-3" :class="{ 'text-center': separator }">
+        <!-- Head -->
+        <div class="flex inline-flex align-middle mb-2">
+          <InfoIcon v-if="icon === 'info'" class="mr-2 text-blue-500" />
+          <StarIcon v-else-if="icon === 'star'" class="mr-2 text-yellow-500" />
+          <DollarSignIcon
+            v-else-if="icon === 'donation'"
+            class="mr-2 text-green-500"
+          />
+          <h1 class="text-lg" v-text="title"></h1>
+        </div>
 
-        <slot name="textRich" />
-        <!-- We can insert extra info here -->
+        <!-- Body -->
+        <div class="text-sm">
+          <p v-if="text" class="mb-1 whitespace-pre-line" v-text="text" />
 
-        <!-- Image -->
-        <img
-          v-if="img"
-          class="mx-auto mt-2"
-          :src="img"
-          :alt="title + ' Example'"
-        />
-        <!-- Links -->
-        <a
-          v-if="link && linkText"
-          :href="link"
-          target="_blank"
-          rel="noopener noreferrer"
-          v-text="linkText"
-        />
-        <!-- Slot for extra info -->
-        <slot />
+          <slot name="textRich" />
+          <!-- We can insert extra info here -->
+
+          <!-- Image -->
+          <img
+            v-if="img"
+            class="mx-auto mt-2"
+            :src="img"
+            :alt="title + ' Example'"
+          />
+          <!-- Links -->
+          <a
+            v-if="link && linkText"
+            :href="link"
+            target="_blank"
+            rel="noopener noreferrer"
+            v-text="linkText"
+          />
+          <!-- Slot for extra info -->
+          <slot />
+        </div>
       </div>
-    </div>
+    </template>
   </div>
 </template>
 
@@ -55,6 +71,9 @@ export default {
   name: "ContentContainer",
   components: { InfoIcon, StarIcon, DollarSignIcon },
   props: {
+    // For separating text
+    separator: { type: Boolean, required: false, default: false },
+    // For normal usage
     title: { type: String, required: true },
     text: { type: String, required: false, default: undefined },
     // For links
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,34 +2,35 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'black-10': 'rgba(0, 0, 0, 0.1)',
-        'black-15': 'rgba(0, 0, 0, 0.15)',
-        'black-80': 'rgba(0, 0, 0, 0.8)',
-        'black-90': 'rgba(0, 0, 0, 0.9)',
-        'white-10': 'rgba(255, 255, 255, 0.1)'
+        "black-10": "rgba(0, 0, 0, 0.1)",
+        "black-15": "rgba(0, 0, 0, 0.15)",
+        "black-80": "rgba(0, 0, 0, 0.8)",
+        "black-90": "rgba(0, 0, 0, 0.9)",
+        "white-10": "rgba(255, 255, 255, 0.1)"
       },
 
       inset: {
-        'minus-1/4': '-25%',
-        'minus-3/4': '-75%',
-        'minus-100': '-100%'
+        "minus-1/4": "-25%",
+        "minus-3/4": "-75%",
+        "minus-100": "-100%"
       },
-
+      // Width and height
       spacing: {
-        'percent-25': '25%'
+        "percent-25": "25%",
+        "fit-content": "fit-content"
       },
 
       maxHeight: {
-        '0': '0px',
-        '1/4': '25%',
-        '1/2': '50%',
-        '3/4': '75%'
+        "0": "0px",
+        "1/4": "25%",
+        "1/2": "50%",
+        "3/4": "75%"
       },
 
       minHeight: {
-        '1/4': '25%',
-        '1/2': '50%',
-        '3/4': '75%'
+        "1/4": "25%",
+        "1/2": "50%",
+        "3/4": "75%"
       }
     }
   },
@@ -39,8 +40,8 @@ module.exports = {
   // Every variant possible
   // variants: ['responsive', 'group-hover', 'focus-within', 'first', 'last', 'odd', 'even', 'hover', 'focus', 'active', 'visited', 'disabled'],
   variants: {
-    textColor: ['responsive', 'group-hover', 'hover', 'focus']
+    textColor: ["responsive", "group-hover", "hover", "focus"]
   }
 
   // plugins: [require('tailwindcss-gradients')()]
-}
+};
