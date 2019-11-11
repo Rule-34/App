df7120370e8d28609ade674da25c689d383734af
Alejandro Akbal <37181533+VoidlessSeven7@users.noreply.github.com>
2019-11-11 16:50:33 +0100

    Improved about page

--- pages/about.vue
+++ pages/about.vue
@@ -4,25 +4,27 @@
     <SideNav :show-search="false" />
     <div class="container md:w-2/3 xl:w-1/2">
       <div class="flex h-perfect p-3 overflow-hidden">
-        <div
+        <a
+          href="https://akbal.dev/"
+          target="_blank"
+          rel="noopener noreferrer"
           class="material-container m-auto p-5 shadow-xl"
           :class="{ zoom: userSettings.zoom.value }"
         >
-          <a
-            href="https://akbal.dev/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+          <div>
             <img class="mx-auto mb-5" src="/img/ALogo.png" alt="Akbal logo" />
-          </a>
-          <div class="text-center">
-            <h1 class="btn">Alejandro Akbal</h1>
-            <small>
+
+            <h1
+              class="py-1 px-3 border rounded-full w-fit-content mx-auto bg-gray-900 text-gray-300 shadow-md mb-1"
+            >
+              Alejandro Akbal
+            </h1>
+            <small class="text-black">
               I'm a web developer and this PWA is a project that I do in my free
               time
             </small>
           </div>
-        </div>
+        </a>
       </div>
 
       <!-- Donation -->
