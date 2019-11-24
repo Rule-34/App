95bd27266599688106876f87ee559fbad215928a
VoidlessSeven7 <37181533+VoidlessSeven7@users.noreply.github.com>
2019-11-24 16:15:28 +0100

    Removed useless SCSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,11 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        "black-10": "rgba(0, 0, 0, 0.1)",
-        "black-15": "rgba(0, 0, 0, 0.15)",
-        "black-80": "rgba(0, 0, 0, 0.8)",
-        "black-90": "rgba(0, 0, 0, 0.9)",
-        "white-10": "rgba(255, 255, 255, 0.1)"
+        // "black-10": "rgba(0, 0, 0, 0.1)",
+        // "black-15": "rgba(0, 0, 0, 0.15)",
+        // "black-80": "rgba(0, 0, 0, 0.8)"
+        // "black-90": "rgba(0, 0, 0, 0.9)",
+        // "white-10": "rgba(255, 255, 255, 0.1)"
       },
 
       inset: {},
