c930735c5220ba187ddc331693c055a7de7adc0e
VoidlessSeven7 <37181533+VoidlessSeven7@users.noreply.github.com>
2019-11-26 09:02:16 +0100

    Revert "Revert "Changing to vuetify""
    This reverts commit cbf32721fda8846d08e4fcd4f000a1e0025cda6a.

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,20 +1,22 @@
+import colors from 'vuetify/es5/util/colors'
+
 export default {
-  mode: "spa",
+  mode: 'spa',
   /*
    ** Headers of the page
    */
   head: {
-    title: "Rule 34 PWA",
+    title: 'Rule 34 PWA',
     meta: [
-      { charset: "utf-8" },
-      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       {
-        hid: "description",
-        name: "description",
-        content: "Rule 34 redesigned in a beautiful and stunning style"
+        hid: 'description',
+        name: 'description',
+        content: 'Rule 34 redesigned in a beautiful and stunning style'
       }
     ],
-    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
   },
   /*
    ** Customize the progress-bar color
@@ -24,9 +26,9 @@ export default {
    ** Customize the splash loading indicator
    */
   loadingIndicator: {
-    name: "cube-grid",
-    color: "white",
-    background: "linear-gradient(152deg, #9b9be0 38%, #00d4ff 100%)"
+    name: 'cube-grid',
+    color: 'white',
+    background: 'linear-gradient(152deg, #9b9be0 38%, #00d4ff 100%)'
   },
   /*
    ** Global CSS
@@ -36,45 +38,66 @@ export default {
    ** Plugins to load before mounting the App
    */
   plugins: [
-    { src: "~/plugins/vuex-persist.js", mode: "client", ssr: false },
-    { src: "~/plugins/v-lazy.js", mode: "client", ssr: false },
-    { src: "~/plugins/insights.js", mode: "client", ssr: false }
+    { src: '~/plugins/vuex-persist.js', mode: 'client', ssr: false },
+    { src: '~/plugins/v-lazy.js', mode: 'client', ssr: false },
+    { src: '~/plugins/insights.js', mode: 'client', ssr: false }
   ],
   /*
    ** Nuxt.js dev-modules
    */
   buildModules: [
     // Doc: https://github.com/nuxt-community/nuxt-tailwindcss
-    "@nuxtjs/tailwindcss"
+    '@nuxtjs/tailwindcss',
+    '@nuxtjs/vuetify'
   ],
   /*
    ** TailwindCSS settings
    */
   tailwindcss: {
-    configPath: "~/tailwind.config.js",
-    cssPath: "~/assets/app.scss"
+    configPath: '~/tailwind.config.js',
+    cssPath: '~/assets/app.scss'
+  },
+  /*
+   ** Vuetify settings
+   */
+  vuetify: {
+    customVariables: ['~/assets/variables.scss'],
+    theme: {
+      dark: true,
+      themes: {
+        dark: {
+          primary: colors.blue.darken2,
+          accent: colors.grey.darken3,
+          secondary: colors.amber.darken3,
+          info: colors.teal.lighten1,
+          warning: colors.amber.base,
+          error: colors.deepOrange.accent4,
+          success: colors.green.accent3
+        }
+      }
+    }
   },
   /*
    ** Nuxt.js modules
    */
   modules: [
     // Doc: https://axios.nuxtjs.org/usage
-    "@nuxtjs/axios",
-    "@nuxtjs/pwa",
-    "nuxt-purgecss"
+    '@nuxtjs/axios',
+    '@nuxtjs/pwa',
+    'nuxt-purgecss'
   ],
   /*
    ** Progressive web app
    */
   pwa: {
     manifest: {
-      name: "Rule 34 PWA",
-      lang: "en",
-      theme_color: "white"
+      name: 'Rule 34 PWA',
+      lang: 'en',
+      theme_color: 'white'
     },
     meta: {
       /* meta options */
-      ogHost: "https://r34.app",
+      ogHost: 'https://r34.app',
       mobileAppIOS: true
     }
   },
@@ -105,4 +128,4 @@ export default {
       // }
     }
   }
-};
+}
