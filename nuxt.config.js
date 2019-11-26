1fa47abd8572a9a6d0a401cc22829356c07d2978
VoidlessSeven7 <37181533+VoidlessSeven7@users.noreply.github.com>
2019-11-26 09:00:07 +0100

    Revert "Converted side nav to vuetify"
    This reverts commit c6f0fc38482bc507abc500b122c722577003daf7.

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,5 @@
+import colors from 'vuetify/es5/util/colors'
+
 export default {
   mode: 'spa',
   /*
@@ -52,7 +54,6 @@ export default {
    ** TailwindCSS settings
    */
   tailwindcss: {
-    defaultAssets: false,
     configPath: '~/tailwind.config.js',
     cssPath: '~/assets/app.scss'
   },
@@ -61,7 +62,20 @@ export default {
    */
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
-    optionsPath: './vuetify.options.js'
+    theme: {
+      dark: true,
+      themes: {
+        dark: {
+          primary: colors.blue.darken2,
+          accent: colors.grey.darken3,
+          secondary: colors.amber.darken3,
+          info: colors.teal.lighten1,
+          warning: colors.amber.base,
+          error: colors.deepOrange.accent4,
+          success: colors.green.accent3
+        }
+      }
+    }
   },
   /*
    ** Nuxt.js modules
