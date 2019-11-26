cbf32721fda8846d08e4fcd4f000a1e0025cda6a
VoidlessSeven7 <37181533+VoidlessSeven7@users.noreply.github.com>
2019-11-26 09:00:14 +0100

    Revert "Changing to vuetify"
    This reverts commit b0913a2b0c64d3a79f5c90d50946f02099a2528c.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,23 @@
 module.exports = {
-  root: true,
-  env: {
-    browser: true,
-    node: true
-  },
-  parserOptions: {
-    parser: 'babel-eslint'
-  },
-  extends: [
-    '@nuxtjs',
-    'prettier',
-    'prettier/vue',
-    'plugin:prettier/recommended',
-    'plugin:nuxt/recommended'
-  ],
-  plugins: [
-    'prettier'
-  ],
-  // add your custom rules here
-  rules: {
-    "vue/component-name-in-template-casing": ["error", "PascalCase"],
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
-  },
-}
+    root: true,
+    env: {
+      node: true
+    },
+    extends: [
+      "plugin:vue/recommended",
+      "eslint:recommended",
+      "prettier/vue",
+      "plugin:prettier/recommended"
+    ],
+    rules: {
+      "vue/component-name-in-template-casing": ["error", "PascalCase"],
+      "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+      "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+    },
+    globals: {
+      $nuxt: true
+    },
+    parserOptions: {
+      parser: "babel-eslint"
+    }
+  };
