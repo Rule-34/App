981da5009e92cbd83707e430994de325a910cdb4
Alejandro Akbal <37181533+VoidlessSeven7@users.noreply.github.com>
2019-12-17 10:28:16 +0100

    Moved code in a more grouped position

--- store/state.js
+++ store/state.js
@@ -4,6 +4,16 @@ export default () => ({
     pid: 0, // Page id
   },
 
+  // These settings are saved to localStorage
+  dashBoardSettings: {
+    contentDomain: 'xxx/',
+    experimentalSettings: false,
+  },
+
+  sideNavData: {
+    isActive: false,
+  },
+
   searchData: {
     data: [], // Data received while searching for tags
     tags: [], // Tags that are added for searching posts with that tags
@@ -12,21 +22,11 @@ export default () => ({
     isFilterActive: false,
   },
 
-  sideNavData: {
-    isActive: false,
-  },
-
   generalData: {
     apiUrl: 'http://localhost:8000/',
     // apiUrl: 'https://r34-app-original-api.herokuapp.com/',
     postLimit: 20,
     errors: null,
-  },
-
-  // These settings are saved to localStorage
-  dashBoardSettings: {
-    contentDomain: 'xxx/',
-    experimentalSettings: false,
   },
 
   // These settings are saved to localStorage
