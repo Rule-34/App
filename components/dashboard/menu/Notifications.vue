cf859364920d109ca7a0a506014618b6a68a4eef
Alejandro Akbal <37181533+VoidlessSeven7@users.noreply.github.com>
2020-01-03 18:07:19 +0100

    Almost got notifications implemented

--- components/dashboard/menu/Notifications.vue
+++ components/dashboard/menu/Notifications.vue
@@ -1,37 +1,33 @@
 <template>
-  <div>
-    <!-- Domain selector -->
-    <div
-      class="inline-flex w-fit-content relative theme-responsive-container rounded-full shadow bg-background overflow-hidden"
-    >
-      <!-- Cloud icon -->
-      <div for="domain-selector" class="inline-flex items-center p-1">
-        <BellIcon class="icon text-primary w-6 h-6" />
-      </div>
+  <!-- Container -->
+  <div
+    class="inline-flex items-center relative overflow-hidden cursor-pointer"
+    @click="notification++"
+  >
+    <!-- Icon -->
+    <div class="theme-responsive-container rounded-full shadow bg-background ">
+      <BellIcon class="icon text-primary w-5 h-5 m-2" />
     </div>
+
+    <!-- Number of notifications -->
+    <div
+      class="absolute top-0 right-0 mr-2 -mt-1 text-sm text-primary select-none"
+      v-text="notification"
+    />
   </div>
 </template>
 
 <script>
-import { mapState, mapActions, mapMutations } from 'vuex'
 import { BellIcon } from 'vue-feather-icons'
 
 export default {
   name: 'Notifications',
   components: { BellIcon },
 
-  computed: {
-    ...mapState(['dashBoardSettings']),
-    selected() {
-      return this.dashBoardSettings.contentDomain
-    },
-  },
-
-  // Experimental features, dont say anything!
-
-  methods: {
-    ...mapMutations(['domainManager', 'pidManager', 'tagManager']),
-    ...mapActions(['getPosts', 'analyticManager']),
+  data() {
+    return {
+      notification: 0,
+    }
   },
 }
 </script>
