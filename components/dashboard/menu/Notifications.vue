216ab6a2d4396b39df2e2414b223ff39ef6d5792
Alejandro Akbal <37181533+VoidlessSeven7@users.noreply.github.com>
2020-01-03 16:21:11 +0100

    Created Notification component

---
+++ components/dashboard/menu/Notifications.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <!-- Domain selector -->
+    <div
+      class="inline-flex w-fit-content relative theme-responsive-container rounded-full shadow bg-background overflow-hidden"
+    >
+      <!-- Cloud icon -->
+      <div for="domain-selector" class="inline-flex items-center p-1">
+        <BellIcon class="icon text-primary w-6 h-6" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapState, mapActions, mapMutations } from 'vuex'
+import { BellIcon } from 'vue-feather-icons'
+
+export default {
+  name: 'Notifications',
+  components: { BellIcon },
+
+  computed: {
+    ...mapState(['dashBoardSettings']),
+    selected() {
+      return this.dashBoardSettings.contentDomain
+    },
+  },
+
+  // Experimental features, dont say anything!
+
+  methods: {
+    ...mapMutations(['domainManager', 'pidManager', 'tagManager']),
+    ...mapActions(['getPosts', 'analyticManager']),
+  },
+}
+</script>
