c46c8521b8e03a1731a235f59ecb6f1e85e87a10
Alejandro Akbal <37181533+VoidlessSeven7@users.noreply.github.com>
2020-01-04 13:35:12 +0100

    Better formatting

--- plugins/v-lazy.js
+++ plugins/v-lazy.js
@@ -1,17 +1,17 @@
-import Vue from "vue";
-import VueLazyload from "vue-lazyload"; // Import lazyloading
+import Vue from 'vue'
+import VueLazyload from 'vue-lazyload' // Import lazyloading
 
 // Lazy load images
 Vue.use(VueLazyload, {
   lazyComponent: true,
   preLoad: 2,
-  error: "/img/error.webp",
+  error: '/img/error.webp',
   // loading: '/icon.png', // TODO: Add skeletonBox
 
   // optional
   observer: true,
   observerOptions: {
-    rootMargin: "0px",
-    threshold: 0.01
-  }
-});
+    rootMargin: '0px',
+    threshold: 0.01,
+  },
+})
