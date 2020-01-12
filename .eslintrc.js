f8198897feaa3773eb6f4059bde49bb8a940b50e
Alejandro Akbal <37181533+VoidlessSeven7@users.noreply.github.com>
2020-01-12 16:00:28 +0100

    Changed configuration due to bugs

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,25 +2,22 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true,
+    node: true
   },
   parserOptions: {
-    parser: 'babel-eslint',
-    sourceType: 'module',
+    parser: 'babel-eslint'
   },
   extends: [
     '@nuxtjs',
     'prettier',
     'prettier/vue',
     'plugin:prettier/recommended',
-    'plugin:nuxt/recommended',
+    'plugin:nuxt/recommended'
   ],
-  plugins: ['prettier'],
+  plugins: [
+    'prettier'
+  ],
   // add your custom rules here
   rules: {
-    'prettier/prettier': 'error',
-    'vue/component-name-in-template-casing': ['error', 'PascalCase'],
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-  },
+  }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,9 @@
+/*
+ ** TailwindCSS Configuration File
+ **
+ ** Docs: https://tailwindcss.com/docs/configuration
+ ** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
+ */
 module.exports = {
   theme: {
     extend: {
@@ -17,29 +23,29 @@ module.exports = {
         'gradient-one': '--color-gradient-one',
         'gradient-two': '--color-gradient-two',
 
-        'black-25': 'rgba(0, 0, 0, 0.25)',
+        'black-25': 'rgba(0, 0, 0, 0.25)'
       },
 
       inset: {},
       // Width and height
       spacing: {
         'percent-25': '25%',
-        'fit-content': 'fit-content',
+        'fit-content': 'fit-content'
       },
 
       maxHeight: {
         '0': '0px',
         '1/4': '25%',
         '1/2': '50%',
-        '3/4': '75%',
+        '3/4': '75%'
       },
 
       minHeight: {
         '1/4': '25%',
         '1/2': '50%',
-        '3/4': '75%',
-      },
-    },
+        '3/4': '75%'
+      }
+    }
   },
   // Recommended variant order
   // ['responsive', 'group-hover', 'focus-within', 'first', 'last', 'odd', 'even', 'hover', 'focus', 'active', 'visited', 'disabled']
@@ -47,6 +53,6 @@ module.exports = {
   // Every variant possible
   // variants: ['responsive', 'group-hover', 'focus-within', 'first', 'last', 'odd', 'even', 'hover', 'focus', 'active', 'visited', 'disabled'],
   variants: {
-    textColor: ['responsive', 'group-hover', 'hover', 'focus'],
-  },
+    textColor: ['responsive', 'group-hover', 'hover', 'focus']
+  }
 }
