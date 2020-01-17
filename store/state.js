47eb2d8d04ebfc7e73d239333ee8e7569d826b20
Alejandro Akbal <37181533+VoidlessSeven7@users.noreply.github.com>
2020-01-17 11:54:15 +0100

    Moved Cors proxy url to state

--- store/state.js
+++ store/state.js
@@ -25,6 +25,7 @@ export default () => ({
   generalData: {
     // apiUrl: 'http://localhost:8000/',
     apiUrl: 'https://r34-app-original-api.herokuapp.com/',
+    corsProxyUrl: 'https://cors-proxy.rule34app.workers.dev/',
     postLimit: 20,
     errors: null
   },
