03abb80405d86eb1c454a5d926fb13b555063c00
Alejandro Akbal <37181533+VoidlessSeven7@users.noreply.github.com>
2020-01-29 19:58:34 +0100

    Removed advertising

--- pages/post.vue
+++ pages/post.vue
@@ -1,23 +1,6 @@
 <template>
   <div>
-    <!-- Advertising test -->
-    <div class="text-center text-default-text-muted mt-3">
-      <small>
-        Testing advertising to maintain the site, sorry for the inconvenience :/
-      </small>
-      <div class="material-container">
-        <iframe
-          src="//a.exosrv.com/iframe.php?idzone=3689087&size=728x90"
-          width="728"
-          height="90"
-          scrolling="no"
-          marginwidth="0"
-          marginheight="0"
-          frameborder="0"
-        />
-      </div>
-    </div>
-
+    
     <!-- If theres errors -->
     <Errors :is-single-post="true" />
 
