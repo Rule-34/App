8d3d9a0fc27fcee12e2dde68ff6d96b89b1418e0
VoidlessSeven7 <37181533+VoidlessSeven7@users.noreply.github.com>
2020-02-09 13:46:06 +0100

    Improved notifications

--- components/components/dashboardMenu/Notifications.vue
+++ components/components/dashboardMenu/Notifications.vue
@@ -1,49 +1,100 @@
 <template>
   <!-- Icon -->
-  <div
-    class="border-util rounded-full shadow bg-elevation p-1"
-    @click="notifications = !notifications"
-  >
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      aria-hidden="true"
-      viewBox="0 0 24 24"
-      class="w-5 h-5"
-    >
-      <g fill="none">
-        <!-- Notification bell -->
-        <path
-          class="icon fill-current text-default-text-muted"
-          d="M15 19a3 3 0 11-6 0h6zm3.13-2H5.87C4.838 17 4 16.105 4 15c0-.348.085-.69.246-.992L6.388 10V8C6.388 4.686 8.9 2 12 2s5.611 2.686 5.611 6v2l2.142 4.008c.513.959.201 2.18-.696 2.728a1.778 1.778 0 01-.928.264z"
-        />
-        <g v-if="notifications">
-          <!-- Bell with a little cut -->
+  <div class="border-util rounded-full shadow bg-elevation relative">
+    <!-- Button -->
+    <button type="button" class="flex p-1" @click="isActive = !isActive">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        aria-hidden="true"
+        viewBox="0 0 24 24"
+        class="w-5 h-5"
+      >
+        <g fill="none">
+          <!-- Notification bell -->
           <path
             class="icon fill-current text-default-text-muted"
-            d="M12.338 2.01a6 6 0 005.274 7.977V10l2.141 4.008c.513.959.201 2.18-.696 2.728a1.778 1.778 0 01-.928.264H5.871C4.837 17 4 16.105 4 15c0-.348.085-.69.246-.992L6.388 10V8C6.388 4.686 8.9 2 12 2c.113 0 .226.004.338.01zM15 19a3 3 0 11-6 0h6z"
+            d="M15 19a3 3 0 11-6 0h6zm3.13-2H5.87C4.838 17 4 16.105 4 15c0-.348.085-.69.246-.992L6.388 10V8C6.388 4.686 8.9 2 12 2s5.611 2.686 5.611 6v2l2.142 4.008c.513.959.201 2.18-.696 2.728a1.778 1.778 0 01-.928.264z"
           />
-          <!-- Notification dot -->
-          <circle
-            cx="18"
-            cy="4.25"
-            r="5"
-            stroke="hsla(205, 78%, 62%, 0.3)"
-            class="icon fill-current text-primary"
-          />
+          <g v-if="notifications">
+            <!-- Bell with a little cut -->
+            <path
+              class="icon fill-current text-default-text-muted"
+              d="M12.338 2.01a6 6 0 005.274 7.977V10l2.141 4.008c.513.959.201 2.18-.696 2.728a1.778 1.778 0 01-.928.264H5.871C4.837 17 4 16.105 4 15c0-.348.085-.69.246-.992L6.388 10V8C6.388 4.686 8.9 2 12 2c.113 0 .226.004.338.01zM15 19a3 3 0 11-6 0h6z"
+            />
+            <!-- Notification dot -->
+            <circle
+              cx="18"
+              cy="4.25"
+              r="4"
+              stroke="hsla(205, 78%, 62%, 0.3)"
+              class="icon fill-current text-primary"
+            />
+          </g>
         </g>
-      </g>
-    </svg>
+      </svg>
+    </button>
+
+    <!-- Content -->
+    <transition name="page">
+      <div v-if="isActive" class="notifications--details">
+        <!-- Donations needed -->
+        <ContentContainer
+          title="Donations needed"
+          text="We have been reaching our daily image limit for some days now, when that happens all images stop loading and so does the App
+
+          If you could donate, even one dollar, we would be able to raise the limit
+          
+          More information on Discord"
+          icon="bg-svg-dollar"
+          icon-position="bg-svg-right"
+          link="https://discord.gg/fUhYHSZ"
+          link-text="Discord"
+        >
+          <!-- Workaround for having two links -->
+          <template>
+            <a class="text-sm" href="https://www.patreon.com/VoidlessSeven7"
+              >Patreon</a
+            >
+            |
+          </template>
+        </ContentContainer>
+      </div>
+    </transition>
   </div>
 </template>
 
 <script>
+import ContentContainer from '~/components/content/ContentContainer.vue'
+
 export default {
   name: 'Notifications',
 
+  components: { ContentContainer },
+
   data() {
     return {
-      notifications: true
+      notifications: true,
+      isActive: false
     }
   }
 }
 </script>
+
+<style>
+.notifications--details {
+  /* Offset a little so its more natural */
+  top: 120%;
+  right: -15px;
+
+  /* Width */
+  min-width: 80vw;
+
+  @apply absolute z-10 border-0 rounded-lg bg-depth;
+}
+
+@screen md {
+  .notifications--details {
+    min-width: 20vw;
+  }
+}
+</style>
