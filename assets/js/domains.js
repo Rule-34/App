eeb856aa79ce03b72b7352c033ef6f249deca290
VoidlessSeven7 <37181533+VoidlessSeven7@users.noreply.github.com>
2020-03-06 13:22:36 +0100

    Implemented use of shared resources

--- assets/js/domains.js
+++ assets/js/domains.js
@@ -1,11 +1,4 @@
-const domains = [
-  { name: 'rule34.xxx', short: 'xxx', pid: 0 },
-  { name: 'rule34.paheal.net', short: 'paheal', pid: 0 },
-  { name: 'danbooru.donmai.us', short: 'danbooru', pid: 1 },
-  { name: 'gelbooru.com', short: 'gelbooru', pid: 0 },
-  { name: 'lolibooru.moe', short: 'loli', pid: 1 },
-  { name: 'e621.net', short: 'e621', pid: 1 }
-]
+import domains from '~/assets/lib/rule-34-shared-resources/domains.json'
 
 function findDomainByShort(domainToSearch) {
   return domains.find((domain) => domain.short === domainToSearch)
