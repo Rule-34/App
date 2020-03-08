516f19d7d1e6d7c52bc021bc5d0f3f7eb0e9cc11
VoidlessSeven7 <37181533+VoidlessSeven7@users.noreply.github.com>
2020-03-08 16:43:50 +0100

    Improved booru tools

--- assets/js/BooruTools.js
+++ assets/js/BooruTools.js
@@ -1,11 +1,18 @@
 import booruList from '~/assets/lib/rule-34-shared-resources/booru-list.json'
 
+// Returns only one
 function findBooruByShort(short) {
   return booruList.find((booru) => booru.short === short)
 }
 
+// Returns an array
 function removeBooruByShort(shortArray) {
   return booruList.filter((booru) => !shortArray.includes(booru.short))
 }
 
-export { booruList, findBooruByShort, removeBooruByShort }
+// Returns an array
+function returnSafeBoorus() {
+  return booruList.filter((booru) => !booru.nsfw)
+}
+
+export { booruList, findBooruByShort, removeBooruByShort, returnSafeBoorus }
