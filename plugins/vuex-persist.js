07df40c8bc536dec2c59d27ce77d150e2863aa0e
VoidlessSeven7 <37181533+VoidlessSeven7@users.noreply.github.com>
2020-03-08 14:29:06 +0100

    Dynamically save only the userSettings value

--- plugins/vuex-persist.js
+++ plugins/vuex-persist.js
@@ -1,11 +1,23 @@
 import createPersistedState from 'vuex-persistedstate'
 
 export default ({ store }) => {
+  const SETTINGS_ARRAY = [
+    'dashBoardSettings',
+    'notificationData'
+    // 'userSettings'
+  ]
+
+  Object.keys(store.state.userSettings).forEach((key) => {
+    SETTINGS_ARRAY.push(`userSettings.${key}.value`)
+  })
+
+  console.log(SETTINGS_ARRAY)
+
   // Hydrate data
   window.onNuxtReady(() => {
     createPersistedState({
       key: 'vuex',
-      paths: ['dashBoardSettings', 'userSettings', 'notificationData']
+      paths: SETTINGS_ARRAY
     })(store)
   })
 }
