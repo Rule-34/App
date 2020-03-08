d0d22133bcf0db749ddc3f330bdf75afc2b7101d
VoidlessSeven7 <37181533+VoidlessSeven7@users.noreply.github.com>
2020-03-08 11:54:40 +0100

    Improved way of using boorus

--- assets/js/BooruTools.js
+++ assets/js/BooruTools.js
@@ -4,5 +4,8 @@ function findBooruByShort(short) {
   return booruList.find((booru) => booru.short === short)
 }
 
+function removeBooruByShort(shortArray) {
+  return booruList.filter((booru) => !shortArray.includes(booru.short))
+}
 
 export { booruList, findBooruByShort, removeBooruByShort }
