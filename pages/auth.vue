a2fb1ece6e940acb3c1815b594009fbf9b03063a
VoidlessSeven7 <37181533+VoidlessSeven7@users.noreply.github.com>
2020-03-22 18:51:01 +0100

    First test of Patreon authentication

---
+++ pages/auth.vue
@@ -0,0 +1,36 @@
+<template>
+  <main>
+    <a
+      :href="`https://www.patreon.com/oauth2/authorize?response_type=code&client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URL}`"
+      >Login with Patreon</a
+    >
+  </main>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      CLIENT_ID:
+        'BR4HkEO0iVPQuQuVdqMzSY_XaTNS0PA_u8BWCLUswjy9dRJ0ZRlasuEf3puxrhTP',
+      REDIRECT_URL:
+        process.env.NODE_ENV === 'development'
+          ? 'http://localhost:8000/oauth/redirect'
+          : 'https://rule-34-api.herokuapp.com/oauth/redirect',
+    }
+  },
+
+  head() {
+    return {
+      title: 'Auth',
+      meta: [
+        {
+          hid: 'usage',
+          name: 'description',
+          content: 'Authenticate to the Rule 34 App with Patreon',
+        },
+      ],
+    }
+  },
+}
+</script>
