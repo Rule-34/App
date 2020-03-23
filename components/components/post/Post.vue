f17ff722f58254f3472cc365d3caf2b37812f3ad
VoidlessSeven7 <37181533+VoidlessSeven7@users.noreply.github.com>
2020-03-23 13:30:51 +0100

    Refactored post component into multiple components
    This fixes hover zoom too

---
+++ components/components/post/Post.vue
@@ -0,0 +1,52 @@
+<template>
+  <!-- Makes element zoomable if setting is enabled -->
+  <figure
+    :class="{ zoom: userSettings.zoom.value }"
+    class="material-container text-default-text text-center"
+  >
+    <!-- Media -->
+    <post-media :post="post" @click.native="isActive = !isActive" />
+
+    <!-- Tags -->
+    <figcaption class="flex flex-wrap overflow-hidden text-sm">
+      <post-tags :tags="post.tags" :is-active="isActive" />
+
+      <!-- Source -->
+      <post-source :source="post.source" />
+    </figcaption>
+  </figure>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+// Components
+import PostMedia from '~/components/components/post/PostMedia.vue'
+import PostTags from '~/components/components/post/PostTags.vue'
+import PostSource from '~/components/components/post/PostSource.vue'
+
+export default {
+  name: 'Post',
+
+  components: { PostMedia, PostTags, PostSource },
+
+  props: {
+    post: {
+      type: Object,
+      default() {
+        return {}
+      },
+    },
+  },
+
+  data() {
+    return {
+      // Internal toggle for showing tags
+      isActive: false,
+    }
+  },
+
+  computed: {
+    ...mapState(['userSettings']),
+  },
+}
+</script>
---
+++ components/components/post/PostMedia.vue
@@ -0,0 +1,122 @@
+<template>
+  <div>
+    <!-- if media is an Image -->
+    <!-- Loading is determined if settings are lazy -->
+    <img
+      v-if="post.type === 'image'"
+      :src="imageSource()"
+      :loading="userSettings.lazyLoading.value ? 'lazy' : 'auto'"
+      :class="{
+        'post-animation': userSettings.animations.value,
+      }"
+      :alt="'Image ' + post.id"
+      class="w-full h-auto"
+      @load="addAnimation($event)"
+      @error="retryToLoadMedia($event)"
+    />
+
+    <!-- if its a Video -->
+    <!-- If lazy loading enabled -->
+    <video
+      v-else-if="post.type === 'video'"
+      :controls="userSettings.videoControls.value"
+      :alt="'Video ' + post.id"
+      class="w-full h-auto"
+      loop
+    >
+      <source :src="post.high_res_file" @error="retryToLoadMedia($event)" />
+      Your browser doesnt support HTML5 video.
+    </video>
+
+    <!-- if its not an image or a video -->
+    <p v-else v-text="'Unknown type of media: ' + post.type" />
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  name: 'PostMedia',
+
+  props: {
+    post: {
+      type: Object,
+      default() {
+        return {}
+      },
+    },
+  },
+
+  data() {
+    return {
+      retryCount: 0,
+    }
+  },
+
+  computed: {
+    ...mapState(['userSettings']),
+  },
+
+  methods: {
+    // Retries to load the image
+    retryToLoadMedia(event) {
+      // console.log(event.target, this.retryCount)
+
+      if (this.retryCount <= this.userSettings.imgRetry.value) {
+        setTimeout(() => {
+          // Save current source
+          const imgSrc = event.target.src
+
+          // Delete source
+          event.target.src = ''
+
+          // Set source again to force reload
+          event.target.src = imgSrc
+
+          // Add one
+          this.retryCount++
+        }, 500 * this.retryCount)
+      } else {
+        // console.log('Cant load the image')
+
+        // Set error image
+        event.target.src = '~/assets/img/utils/error.png'
+
+        // Stop retrying
+        event.target.onerror = null
+      }
+    },
+
+    // Image source
+    imageSource() {
+      // Return full image if it exists AND its setting is enabled
+      if (this.userSettings.fullSizeImages.value && this.post.high_res_file) {
+        return this.post.high_res_file
+
+        // Else return low res file
+      } else {
+        return this.post.low_res_file
+      }
+    },
+
+    addAnimation(event) {
+      if (this.userSettings.animations.value) {
+        event.target.classList.toggle('post-animation--active')
+      }
+    },
+  },
+}
+</script>
+
+<style>
+/* Invisible until it loads */
+.post-animation {
+  transition: opacity 0.75s;
+  @apply opacity-0;
+}
+
+.post-animation--active {
+  @apply opacity-100;
+}
+</style>
