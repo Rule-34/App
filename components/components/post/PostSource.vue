888b0b2dabd3b97de308e5d4d195756f4cd06bbe
VoidlessSeven7 <37181533+VoidlessSeven7@users.noreply.github.com>
2020-03-23 13:40:47 +0100

    Use SVG directly instead of a component

--- components/components/post/PostSource.vue
+++ components/components/post/PostSource.vue
@@ -10,7 +10,22 @@
       target="_blank"
     >
       <p class="color-util" v-text="'Source'" />
-      <ExternalLinkIcon class="icon text-default w-5 h-5 ml-2" />
+
+      <!-- Link Icon -->
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        opacity="0.8"
+        fill="none"
+        stroke="currentColor"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="icon text-default w-5 h-5 ml-2"
+      >
+        <path
+          d="M18 13v6a2 2 0 01-2 2H5a2 2 0 01-2-2V8a2 2 0 012-2h6M15 3h6v6M10 14L21 3"
+        />
+      </svg>
     </a>
 
     <!-- If the text is not a url then just show the text -->
@@ -19,13 +34,8 @@
 </template>
 
 <script>
-// Third party
-import { ExternalLinkIcon } from 'vue-feather-icons'
-
 export default {
   name: 'PostSource',
-
-  components: { ExternalLinkIcon },
 
   props: {
     source: {
