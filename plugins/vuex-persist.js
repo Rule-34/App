b1fcedf2514393581693ffb631510a1ce44d6c35
VoidlessSeven7 <37181533+VoidlessSeven7@users.noreply.github.com>
2020-03-24 22:23:17 +0100

    Save patreon data to localStorage

--- plugins/vuex-persist.js
+++ plugins/vuex-persist.js
@@ -8,6 +8,9 @@ export default ({ store }) => {
     'notificationData.data',
     'notificationData.latestTitle',
     // 'userSettings'
+
+    // Patreon data
+    'patreonCredentials',
   ]
 
   // Push every setting's value path to save them to localStorage (this way we dont save other data like titles, defaultValue, etc.)
