6bd12eaf12349662134d363661fcc6c7631d3dc2
VoidlessSeven7 <37181533+VoidlessSeven7@users.noreply.github.com>
2020-03-25 11:38:26 +0100

    Removed onNuxtReady as its more of a hassle than anything

--- plugins/vuex-persist.js
+++ plugins/vuex-persist.js
@@ -2,38 +2,36 @@ import VuexPersistence from 'vuex-persist'
 
 export default ({ store }) => {
   // Hydrate data
-  window.onNuxtReady(() => {
-    new VuexPersistence({
-      reducer: (state) => {
-        const SETTINGS_OBJ = { userSettings: {} }
+  new VuexPersistence({
+    reducer: (state) => {
+      const SETTINGS_OBJ = { userSettings: {} }
 
-        // Recreate every setting's value path to save them to localStorage (this way we dont save other data like titles, defaultValue, etc.)
-        Object.keys(state.userSettings).forEach((key) => {
-          SETTINGS_OBJ.userSettings[key] = {
-            value: state.userSettings[key].value,
-          }
-        })
-
-        // console.log(SETTINGS_OBJ)
-
-        // Recreate the part of the store that we want to save
-        return {
-          // Dashboard
-          dashBoardSettings: state.dashBoardSettings,
-
-          // Notifications
-          notificationData: {
-            data: state.notificationData.data,
-            latestTitle: state.notificationData.latestTitle,
-          },
-
-          // userSettings
-          ...SETTINGS_OBJ,
-
-          // Patreon data
-          patreonCredentials: state.patreonCredentials,
+      // Recreate every setting's value path to save them to localStorage (this way we dont save other data like titles, defaultValue, etc.)
+      Object.keys(state.userSettings).forEach((key) => {
+        SETTINGS_OBJ.userSettings[key] = {
+          value: state.userSettings[key].value,
         }
-      },
-    }).plugin(store)
-  })
+      })
+
+      // console.log(SETTINGS_OBJ)
+
+      // Recreate the part of the store that we want to save
+      return {
+        // Dashboard
+        dashBoardSettings: state.dashBoardSettings,
+
+        // Notifications
+        notificationData: {
+          data: state.notificationData.data,
+          latestTitle: state.notificationData.latestTitle,
+        },
+
+        // userSettings
+        ...SETTINGS_OBJ,
+
+        // Patreon data
+        patreonCredentials: state.patreonCredentials,
+      }
+    },
+  }).plugin(store)
 }
