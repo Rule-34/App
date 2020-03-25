f2a7d69df09701762137316e2bc8fdcc60f5629e
VoidlessSeven7 <37181533+VoidlessSeven7@users.noreply.github.com>
2020-03-25 18:21:22 +0100

    Improved auth design

--- pages/auth.vue
+++ pages/auth.vue
@@ -18,16 +18,57 @@
 
     <!-- If user is authenticated -->
     <template v-else>
-      <div class="material-container text-default-text flex flex-wrap p-3">
-        <img
-          :src="patreonCredentials.image_url"
-          alt="User image"
-          class="h-12 w-12"
-        />
-        <h3
-          class="text-lg my-auto sm:ml-2"
-          v-text="patreonCredentials.full_name"
-        />
+      <!-- User card -->
+      <div
+        class="material-container overflow-visible text-default-text p-3 mt-20 relative"
+      >
+        <!-- Image -->
+        <div
+          class="absolute -top-14 h-28 w-28 md:-top-16 md:h-32 md:w-32 inset-x-0 mx-auto bg-background border-util rounded-full overflow-hidden"
+        >
+          <img
+            :src="patreonCredentials.image_url"
+            alt="User image"
+            class="w-full h-auto"
+          />
+        </div>
+        <!-- Name -->
+        <div class="text-center mt-14 md:mt-16">
+          <h3
+            class="text-lg md:text-2xl"
+            v-text="patreonCredentials.full_name"
+          />
+        </div>
+
+        <div>
+          <h3>Tier</h3>
+          <h3>Subscriber since</h3>
+        </div>
+      </div>
+
+      <!-- Log out -->
+      <div class="ml-auto mr-5">
+        <button
+          class="color-util border-util bg-elevation rounded-full py-2 px-4 mt-3"
+          @click="patreonManager({ mode: 'reset' })"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="icon h-5 w-5 text-default inline -mt-1"
+          >
+            <path
+              d="M9 21H5a2 2 0 01-2-2V5a2 2 0 012-2h4M16 17l5-5-5-5M21 12H9"
+            />
+          </svg>
+
+          Log out
+        </button>
       </div>
     </template>
   </main>
