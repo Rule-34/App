81d5b659b87a6c7935588a413e4bc1e6b69b125b
Alejandro Akbal <37181533+VoidlessSeven7@users.noreply.github.com>
2020-04-17 20:14:48 +0200

    Force notifications to update

--- store/actions.js
+++ store/actions.js
@@ -113,7 +113,7 @@ export default {
         // Fetch data
         response = await dispatch('simpleFetch', {
           url:
-            'https://cdn.statically.io/gist/VoidlessSeven7/2fe43e0eee40be63d9b2a582b2793cf9/raw/app-notifications.min.json',
+            'https://cdn.statically.io/gist/VoidlessSeven7/2fe43e0eee40be63d9b2a582b2793cf9/raw/app-notifications.json',
         })
 
         // Set mutation to return
