5ee51331e5f45bbb4ee2278f20a21cbac7bd64b6
Alejandro Akbal <37181533+VoidlessSeven7@users.noreply.github.com>
2020-04-27 12:29:55 +0200

    Reverted extraction of svg

--- components/components/post/PostSource.vue
+++ components/components/post/PostSource.vue
@@ -12,20 +12,7 @@
       <p class="color-util" v-text="'Source'" />
 
       <!-- Link Icon -->
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 24 24"
-        opacity="0.8"
-        fill="none"
-        stroke="currentColor"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        class="icon text-default w-5 h-5 ml-2"
-      >
-        <path
-          d="M18 13v6a2 2 0 01-2 2H5a2 2 0 01-2-2V8a2 2 0 012-2h6M15 3h6v6M10 14L21 3"
-        />
-      </svg>
+      <ExternalLinkIcon class="icon text-default w-5 h-5 ml-2" />
     </a>
 
     <!-- If the text is not a url then just show the text -->
@@ -34,8 +21,12 @@
 </template>
 
 <script>
+import { ExternalLinkIcon } from 'vue-feather-icons'
+
 export default {
   name: 'PostSource',
+
+  components: { ExternalLinkIcon },
 
   props: {
     source: {
