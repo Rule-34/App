527da193f0fec2300511671337d40e5e38b3d20a
Alejandro Akbal <37181533+VoidlessSeven7@users.noreply.github.com>
2020-05-14 16:27:08 +0200

    Updated actions.js to use the new API and componentised it

---
+++ assets/js/createAPIURLFromBooruData.js
@@ -0,0 +1,53 @@
+export function createAPIURL(mode, state, parameters = {}) {
+  const domainData = state.booruData.active
+
+  const queryObj = {
+    posts: {
+      limit: state.userSettings.postsPerPage.value,
+      pid: state.booruData.active.pid,
+      tags: state.searchData.tags.join('+'),
+      score: state.userSettings.score.value,
+    },
+
+    singlePost: {
+      id: parameters.postID,
+    },
+
+    tags: { tag: parameters.tag },
+  }
+
+  let URL = state.generalData.apiUrl
+
+  switch (mode) {
+    case 'posts':
+      URL += `booru/${domainData.type}/${mode}?domain=${domainData.domain}`
+
+      URL += '&limit=' + queryObj.posts.limit
+
+      URL += '&pid=' + queryObj.posts.pid
+
+      URL += '&tags=' + queryObj.posts.tags
+
+      URL += '&score=>=' + queryObj.posts.score
+      break
+
+    case 'single-post':
+      URL += `booru/${domainData.type}/${mode}?domain=${domainData.domain}`
+
+      URL += '&id=' + queryObj.singlePost.id
+      break
+
+    case 'tags':
+      URL += `booru/${domainData.type}/${mode}?domain=${domainData.domain}`
+
+      // URL += '&limit=' + queryObj.limit
+
+      URL += '&tag=' + queryObj.tags.tag
+      break
+
+    default:
+      throw new Error('No mode specified')
+  }
+
+  return URL
+}
