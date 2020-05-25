5091778483fc4f92101a870d7f30d8d412711869
Alejandro Akbal <37181533+VoidlessSeven7@users.noreply.github.com>
2020-05-25 15:25:20 +0200

    Reflect Domain and PID in the URL queries

--- store/getters.js
+++ store/getters.js
@@ -4,6 +4,11 @@ import {
 } from '~/assets/lib/rule-34-shared-resources/util/BooruUtils.js'
 
 export default {
+  getActiveBooru: (state) => {
+    // Preparing to change this logic and improve it
+    return state.booruData.active
+  },
+
   getActiveBooruType: (state) => {
     return findBoorusWithValueByKey(
       state.booruData.active.type,
