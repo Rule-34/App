643636abf98c18a2107a3b4e9c5d15941968d163
Alejandro Akbal <37181533+VoidlessSeven7@users.noreply.github.com>
2020-07-14 13:18:48 +0200

    refactor: rename component and move logic

--- pages/premium.vue
+++ pages/premium.vue
@@ -2,6 +2,24 @@
 <template>
   <main class="flex h-screen">
     <template v-if="hasValidLicenseKey">
+      <div class="flex flex-col w-full py-4">
+        <PremiumDashboard />
+
+        <!-- Spacer -->
+        <div class="flex-grow" />
+
+        <!-- Notice -->
+        <p class="text-sm text-center text-default-text-muted">
+          Manage your subscription on
+          <a
+            rel="noopener nofollow"
+            target="_blank"
+            href="https://gumroad.com/library"
+          >
+            Gumroad
+          </a>
+        </p>
+      </div>
     </template>
 
     <template v-else>
@@ -26,12 +44,12 @@ import { mapGetters } from 'vuex'
 // Components
 import Login from '@/components/pages/premium/Login'
 import Subscription from '@/components/pages/premium/Subscription'
-import ControlPannel from '@/components/pages/premium/ControlPannel'
+import PremiumDashboard from '@/components/pages/premium/PremiumDashboard'
 
 import Errors from '~/components/utils/Errors.vue'
 
 export default {
-  components: { Login, Subscription, Errors, ControlPannel },
+  components: { Login, Subscription, Errors, PremiumDashboard },
 
   computed: {
     ...mapGetters('premium', ['hasValidLicenseKey']),
