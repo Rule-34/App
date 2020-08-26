2d67b5eddcbf224d3fb94b855a6a2febc9d241b1
Alejandro Akbal <37181533+VoidlessSeven7@users.noreply.github.com>
2020-08-26 16:02:04 +0200

    feat: improve Error component to be more dynamic

--- pages/index.vue
+++ pages/index.vue
@@ -8,7 +8,7 @@
     </div>
 
     <!-- Content -->
-    <Errors />
+    <ErrorManager />
 
     <Post v-for="post in posts.data" :key="post.id" :post="post" />
 
@@ -22,14 +22,14 @@ import { mapState } from 'vuex'
 // Components
 import DomainSelector from '~/components/pages/posts/domain/Selector.vue'
 import Post from '~/components/pages/posts/content/Post.vue'
-import Errors from '~/components/utils/Errors.vue'
+import ErrorManager from '~/components/utils/ErrorManager.vue'
 
 // Mixins
 import PostsStartupMixin from '~/components/pages/posts/util/PostsStartupMixin.js'
 
 export default {
   components: {
-    Errors,
+    ErrorManager,
     DomainSelector,
     Notifications: () =>
       import('~/components/pages/posts/navigation/Notifications.vue'),
