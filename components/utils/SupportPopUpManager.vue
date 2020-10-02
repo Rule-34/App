fc6c47dfcc44bfe7b16dc4c062c3fbbe723d1f4e
Alejandro Akbal <37181533+VoidlessSeven7@users.noreply.github.com>
2020-10-02 12:07:12 +0200

    feat: create support pop up logic

---
+++ components/utils/SupportPopUpManager.vue
@@ -0,0 +1,38 @@
+<script>
+import { mapGetters, mapMutations } from 'vuex'
+
+export default {
+  data() {
+    return {
+      isSupportPopUpActive: false,
+    }
+  },
+
+  computed: {
+    ...mapGetters(['getTimesTheAppHasBeenOpened']),
+  },
+
+  watch: {
+    getTimesTheAppHasBeenOpened(value) {
+      if (value % 10 === 0) {
+        this.isSupportPopUpActive = true
+        //
+      } else {
+        this.isSupportPopUpActive = false
+      }
+    },
+  },
+
+  mounted() {
+    this.setTimesTheAppHasBeenOpened(this.getTimesTheAppHasBeenOpened + 1)
+  },
+
+  methods: {
+    ...mapMutations(['setTimesTheAppHasBeenOpened']),
+
+    toggleSupportPopUp() {
+      this.isSupportPopUpActive = !this.isSupportPopUpActive
+    },
+  },
+}
+</script>
