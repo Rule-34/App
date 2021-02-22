d0024edf57d41a731601b4997e35979838411c37
Alejandro Akbal <37181533+VoidlessSeven7@users.noreply.github.com>
2021-02-22 19:48:07 +0100

    refactor: rewrite layout

--- pages/about.vue
+++ pages/about.vue
@@ -1,35 +1,37 @@
 <template>
-  <main class="flex h-screen p-3">
-    <a
-      href="https://akbal.dev/"
-      target="_blank"
-      rel="noopener"
-      class="p-5 m-auto shadow-md material-container bg-elevation"
-    >
-      <div>
+  <div class="flex flex-col justify-around h-screen">
+    <main class="flex flex-auto">
+      <div class="p-5 m-auto shadow-md material-container bg-elevation">
         <picture>
           <source srcset="~/assets/img/brand/akbal.webp" type="image/webp" />
+
           <img
-            class="mx-auto mb-5"
             src="~/assets/img/brand/akbal.png"
             alt="Akbal logo"
             loading="eager"
             height="206"
             width="231"
+            class="block mx-auto"
           />
         </picture>
-        <h1
-          class="px-3 py-1 mx-auto mb-1 rounded-full shadow-md material-container w-max-content bg-background"
+
+        <a
+          href="https://akbal.dev/"
+          target="_blank"
+          rel="noopener"
+          class="block px-3 py-1 mx-auto mt-5 mb-2 rounded-full shadow-md material-container w-max-content bg-background"
         >
           Alejandro Akbal
-        </h1>
-        <small class="text-default-text">
-          I'm a web developer and this App is a project that I do in my free
-          time
+        </a>
+
+        <small class="block text-default-text">
+          I'm a software developer and this App is a project that I do in my
+          free time.
         </small>
       </div>
-    </a>
-  </main>
+    </main>
+
+  </div>
 </template>
 
 <script>
