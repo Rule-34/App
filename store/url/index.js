fd0e3fec3bb7ebf08cb167c5f4b6c7796611809c
Alejandro Akbal <37181533+VoidlessSeven7@users.noreply.github.com>
2021-02-22 13:46:41 +0100

    feat!: rewrite vuex logic to set the source of truth the URL queries
    BREAKING CHANGE: the source of truth is now the URL

---
+++ store/url/index.js
@@ -0,0 +1,57 @@
+export const getters = {
+  urlDomain(state, getters, rootState, rootGetters) {
+    const { domain } = rootState.route.query
+
+    return domain
+  },
+
+  urlPage(state, getters, rootState, rootGetters) {
+    const { page } = rootState.route.query
+
+    return page
+  },
+
+  urlTags(state, getters, rootState, rootGetters) {
+    const { tags } = rootState.route.query
+
+    return tags
+  },
+}
+
+export const actions = {
+  async setInitialUrlState(context) {
+    const { dispatch } = context
+
+    await dispatch('pushRouteQueries', {})
+  },
+
+  async pushRouteQueries(context, { domain, page, tags }) {
+    const { rootGetters } = context
+
+    if (domain === undefined) {
+      domain = rootGetters['booru/getActiveBooru'].domain
+    }
+
+    if (page === undefined) {
+      page = rootGetters['booru/getPageID']
+    }
+
+    if (tags === undefined) {
+      tags = rootGetters['booru/getTags']
+    }
+
+    const routerQueries = {
+      query: {
+        domain,
+        page,
+        ...(tags.length && {
+          tags: tags.join(','),
+        }),
+      },
+    }
+
+    console.log(routerQueries)
+
+    await this.$router.push(routerQueries)
+  },
+}
