812d7d00383bdb27a559db0638f41a29ab5bfce6
Alejandro Akbal <37181533+VoidlessSeven7@users.noreply.github.com>
2021-02-23 23:24:03 +0100

    refactor(toggler): improve layout

--- components/layout/navigation/sidenav/Toggler.vue
+++ components/layout/navigation/sidenav/Toggler.vue
@@ -1,35 +1,50 @@
 <template>
   <!-- Side Nav Toggler -->
-  <div class="fixed z-50 navigation-toggler">
-    <button
-      class="block w-12 h-12 m-4 border-0 rounded-full shadow-lg select-none md:w-16 md:h-16 md:m-2 bg-gradient-lilac-blue"
-      type="menu"
-      title="Menu"
-      @click="toggleSideNav()"
-    >
-      &nbsp;
-    </button>
-    <button
-      v-show="showSearch"
-      type="menu"
-      class="block w-10 h-10 m-5 border-0 rounded-full shadow-lg select-none md:w-12 md:h-12 md:m-4 bg-gradient-lilac-blue"
-      title="Search"
-      @click="toggleSearch()"
-    >
-      <!-- Search Icon -->
-      <SearchIcon class="inline-flex w-6 h-6 icon text-default" />
-    </button>
-  </div>
+  <aside class="fixed z-50 navigation-toggler">
+    <div class="flex flex-col items-center justify-center">
+      <button
+        class="flex items-center justify-center w-12 h-12 p-4 border-0 border-transparent rounded-full shadow-lg md:w-16 md:h-16 bg-gradient-lilac-blue"
+        type="menu"
+        title="Menu"
+        aria-label="Toggle the Menu"
+        @click="toggleSideNav"
+      >
+        &nbsp;
+      </button>
+
+      <div class="my-2"></div>
+
+      <button
+        v-show="showSearch"
+        type="menu"
+        class="flex items-center justify-center w-10 h-10 p-2 border-0 rounded-full shadow-lg md:w-12 md:h-12 bg-gradient-lilac-blue"
+        title="Search"
+        aria-label="Toggle the search menu"
+        @click="toggleSearch"
+      >
+        <!-- Search Icon -->
+        <svg
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="1.6"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          class="text-default"
+        >
+          <circle cx="11" cy="11" r="8"></circle>
+          <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
+        </svg>
+      </button>
+    </div>
+  </aside>
 </template>
 
 <script>
 import { mapActions } from 'vuex'
-import { SearchIcon } from 'vue-feather-icons'
 
 export default {
   name: 'NavToggler',
-
-  components: { SearchIcon },
 
   props: { showSearch: { type: Boolean, default: false, required: false } },
 
@@ -52,16 +67,16 @@ export default {
 
 <style lang="postcss">
 .navigation-toggler {
-  top: 7vh;
-  transform: translateX(-35px);
   @apply transition-transform duration-300;
+
+  top: 7vh;
+  transform: translateX(-50%);
 }
 
 .navigation-toggler:hover {
   transform: translateX(1vw);
 }
 
-/* Changes for medium screens */
 @screen md {
   .navigation-toggler {
     transform: translateX(1vw);
