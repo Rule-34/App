0280e6d8403282fa5044688ca9b6404421301294
Alejandro Akbal <37181533+VoidlessSeven7@users.noreply.github.com>
2021-02-24 18:51:53 +0100

    refactor(about page): apply layout constraints and improvements

--- pages/about.vue
+++ pages/about.vue
@@ -1,37 +1,39 @@
 <template>
-  <div class="flex flex-col justify-around h-screen">
-    <main class="flex flex-auto">
-      <div class="p-5 m-auto shadow-md material-container bg-elevation">
-        <picture>
-          <source srcset="~/assets/img/brand/akbal.webp" type="image/webp" />
+  <div
+    class="container relative flex flex-col items-center justify-center min-h-screen px-4 mx-auto sm:px-6 lg:px-8"
+  >
+    <main class="p-5 rounded-lg shadow-md bg-elevation border-util">
+      <picture>
+        <source srcset="~/assets/img/brand/akbal.webp" type="image/webp" />
 
-          <img
-            src="~/assets/img/brand/akbal.png"
-            alt="Akbal logo"
-            loading="eager"
-            height="206"
-            width="231"
-            class="block mx-auto"
-          />
-        </picture>
+        <img
+          src="~/assets/img/brand/akbal.png"
+          alt="Akbal logo"
+          loading="eager"
+          height="206"
+          width="231"
+          class="mx-auto"
+        />
+      </picture>
 
-        <a
-          href="https://akbal.dev/"
-          target="_blank"
-          rel="noopener"
-          class="block px-3 py-1 mx-auto mt-5 mb-2 rounded-full shadow-md material-container w-max-content bg-background"
-        >
-          Alejandro Akbal
-        </a>
+      <a
+        href="https://akbal.dev/"
+        target="_blank"
+        rel="noopener"
+        class="block px-3 py-1 mx-auto mt-5 mb-2 rounded-full shadow-md border-util w-max-content bg-background"
+      >
+        Alejandro Akbal
+      </a>
 
-        <small class="block text-default-text">
-          I'm a software developer and this App is a project that I do in my
-          free time.
-        </small>
-      </div>
+      <small class="block text-default-text">
+        I'm a software developer and this App is a project that I do in my free
+        time.
+      </small>
     </main>
 
-    <footer class="p-2 text-xs text-center text-default-text-muted">
+    <footer
+      class="absolute inset-x-0 bottom-0 p-2 text-xs text-center text-default-text-muted"
+    >
       v{{ app_version }}
     </footer>
   </div>
