3a372d4d7b67157d3ba6b53eff2bef6fd58e21bc
Alejandro Akbal <37181533+AlejandroAkbal@users.noreply.github.com>
2021-04-08 13:50:34 +0200

    refactor: improve structure

--- components/pages/premium/PremiumSubscription.vue
+++ components/pages/premium/PremiumSubscription.vue
@@ -77,16 +77,14 @@
       </ul>
 
       <div class="mt-8">
-        <div class="rounded-lg">
-          <a
-            target="_blank"
-            rel="noopener"
-            href="https://gum.co/Rule34App"
-            class="block w-full px-6 py-3 text-lg font-medium leading-6 text-center text-black transition border-0 rounded-lg bg-gradient-to-r from-accent-400 to-primary-400 focus:focus-util hover:text-gray-800"
-          >
-            Subscribe
-          </a>
-        </div>
+        <a
+          target="_blank"
+          rel="noopener"
+          href="https://gum.co/Rule34App"
+          class="block w-full px-6 py-3 text-lg font-medium leading-6 text-center text-black transition border-0 rounded-lg bg-gradient-to-r from-accent-400 to-primary-400 focus:focus-util hover:text-gray-800"
+        >
+          Subscribe
+        </a>
       </div>
     </div>
   </div>
