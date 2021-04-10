bc563c30097d4b3029588eecc9a7f28f30fd8fed
Alejandro Akbal <37181533+AlejandroAkbal@users.noreply.github.com>
2021-04-10 12:05:40 +0200

    Revert "chore: remove postsPerPage user setting to enable better cache"
    This reverts commit 4dca00241d19d5e41868ac8dcf89638fc998ae5b.

--- store/user.js
+++ store/user.js
@@ -65,6 +65,13 @@ export const state = () => ({
       defaultValue: false,
     },
 
+    postsPerPage: {
+      name: 'Posts per page',
+      description: 'Number of posts to load per page, hard limit is 100.',
+      value: 20,
+      defaultValue: 20,
+    },
+
     score: {
       name: 'Minimum score',
       description: 'Required score for a post to show.',
