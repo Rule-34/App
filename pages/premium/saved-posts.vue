e25516abd948afc8cb45d2add653f7a2820e6a02
Alejandro Akbal <37181533+AlejandroAkbal@users.noreply.github.com>
2021-06-08 19:10:54 +0200

    feat: add booru selector

--- pages/premium/saved-posts.vue
+++ pages/premium/saved-posts.vue
@@ -2,15 +2,30 @@
   <main class="flex flex-col max-w-3xl min-h-screen p-4 mx-auto sm:p-6 lg:p-8">
     <ContentSeparator title="Saved posts" />
 
+    <select
+      id="booru-selector"
+      name="booru"
+      aria-label="Change the domain where the saved posts are shown"
+      @change="booruChangeListener"
+    >
+      <option value="all" selected>&lt;All&gt;</option>
+
+      <template v-for="booru in availableBoorusWithSavedPosts">
+        <option :value="booru" :key="booru" :selected="selectedBooru === booru">
+          {{ booru }}
+        </option>
+      </template>
+    </select>
+
     <ul class="mt-4 space-y-4">
       <!-- TODO: separate with title if they are from different boorus -->
       <!-- TODO: use the same booru by the default that is active -->
 
-      <template v-if="savedPostsFromCurrentBooru.length">
-        <li v-for="post in savedPostsFromCurrentBooru" :key="post.data.id">
+      <template v-if="savedPostsFromSelectedBooru.length">
+        <li v-for="POST in savedPostsFromSelectedBooru" :key="POST.data.id">
           <Post
-            :post-domain="getActiveBooru.domain"
-            :post-data="post.data"
+            :post-domain="POST.meta_data.booru_domain"
+            :post-data="POST.data"
             :view-only="true"
           />
         </li>
