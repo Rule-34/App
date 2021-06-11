f6ee5a00b1a2d5ab874c8eabfa04a82cc801b6fe
Alejandro Akbal <37181533+AlejandroAkbal@users.noreply.github.com>
2021-06-11 11:25:35 +0200

    refactor: make component more flexible

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,11 @@
   >
     <!-- Top menu -->
     <nav class="flex flex-row items-center justify-between py-4">
-      <DomainSelector />
+      <DomainSelector
+        :activeDomain="getActiveBooru.domain"
+        :domainGroupList="booruGroupList"
+        @domainChange="onDomainChange"
+      />
 
       <Notifications />
     </nav>
@@ -25,7 +29,7 @@
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
+import { mapGetters, mapActions } from 'vuex'
 
 // Mixins
 import UrlManagerMixin from '~/components/pages/posts/navigation/url/UrlManagerMixin.js'
@@ -34,7 +38,55 @@ export default {
   mixins: [UrlManagerMixin],
 
   computed: {
-    ...mapGetters('booru', ['getActiveBooru', 'getPosts']),
+    ...mapGetters('booru', [
+      'getActiveBooru',
+      'getDefaultBooruList',
+      'getPremiumBooruList',
+      'getPosts',
+    ]),
+    ...mapGetters('premium', ['isUserPremium']),
+
+    booruGroupList() {
+      const BOORU_LIST = []
+
+      // Default boorus
+      const DOMAINS_FROM_DEFAULT_BOORU_LIST = this.getDefaultBooruList.map(
+        (BOORU) => BOORU.domain
+      )
+      const DEFAULT_BOORU_LIST = {
+        name: 'Default',
+        domains: DOMAINS_FROM_DEFAULT_BOORU_LIST,
+      }
+
+      BOORU_LIST.push(DEFAULT_BOORU_LIST)
+
+      // Premium boorus
+      const DOMAINS_FROM_PREMIUM_BOORU_LIST = this.getPremiumBooruList.map(
+        (BOORU) => BOORU.domain
+      )
+      const PREMIUM_BOORU_LIST = {
+        name: 'Custom',
+        domains: [...DOMAINS_FROM_PREMIUM_BOORU_LIST, '<Add Booru>'],
+      }
+
+      if (this.isUserPremium) {
+        BOORU_LIST.push(PREMIUM_BOORU_LIST)
+      }
+      return BOORU_LIST
+    },
+  },
+
+  methods: {
+    ...mapActions('booru', ['activeBooruManager']),
+
+    async onDomainChange(domain) {
+      if (domain === '<Add Booru>') {
+        await this.$router.push({ name: 'premium' })
+        return
+      }
+
+      await this.activeBooruManager({ operation: 'set', value: domain })
+    },
   },
 }
 </script>
