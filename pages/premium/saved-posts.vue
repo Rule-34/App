c342307c6e7003248c18e0c3ad49b8ab5c629106
Alejandro Akbal <37181533+AlejandroAkbal@users.noreply.github.com>
2021-06-11 11:26:31 +0200

    feat(saved posts): implement DomainSelector component

--- pages/premium/saved-posts.vue
+++ pages/premium/saved-posts.vue
@@ -2,25 +2,15 @@
   <main class="flex flex-col max-w-3xl min-h-screen p-4 mx-auto sm:p-6 lg:p-8">
     <ContentSeparator title="Saved posts" />
 
-    <select
-      id="booru-selector"
-      name="booru"
-      aria-label="Change the domain where the saved posts are shown"
-      @change="booruChangeListener"
-    >
-      <option value="all" selected>&lt;All&gt;</option>
+    <nav class="flex flex-row items-center justify-between py-4">
+      <DomainSelector
+        :activeDomain="selectedBooru"
+        :domainGroupList="boorusThatHaveSavedPosts"
+        @domainChange="onDomainChange"
+      />
+    </nav>
 
-      <template v-for="booru in availableBoorusWithSavedPosts">
-        <option :value="booru" :key="booru" :selected="selectedBooru === booru">
-          {{ booru }}
-        </option>
-      </template>
-    </select>
-
-    <ul class="mt-4 space-y-4">
-      <!-- TODO: separate with title if they are from different boorus -->
-      <!-- TODO: use the same booru by the default that is active -->
-
+    <ul class="space-y-4">
       <template v-if="savedPostsFromSelectedBooru.length">
         <li v-for="POST in savedPostsFromSelectedBooru" :key="POST.data.id">
           <Post
@@ -66,6 +56,20 @@ export default {
   computed: {
     ...mapGetters('user', ['getSavedPosts']),
 
+    boorusThatHaveSavedPosts() {
+      const BOORU_DOMAINS = this.getSavedPosts.map(
+        (POST) => POST.meta_data.booru_domain
+      )
+
+      const UNIQUE_BOORU_DOMAINS = [...new Set(BOORU_DOMAINS)]
+
+      const DOMAIN_GROUP_LIST = [
+        { name: 'Default', domains: ['<All Boorus>', ...UNIQUE_BOORU_DOMAINS] },
+      ]
+
+      return DOMAIN_GROUP_LIST
+    },
+
     savedPostsFromSelectedBooru() {
       let SAVED_POSTS = null
 
@@ -86,25 +90,11 @@ export default {
 
       return SORTED_SAVED_POSTS
     },
-
-    availableBoorusWithSavedPosts() {
-      const BOORU_DOMAINS = this.getSavedPosts.map(
-        (POST) => POST.meta_data.booru_domain
-      )
-
-      const UNIQUE_BOORU_DOMAINS = [...new Set(BOORU_DOMAINS)]
-
-      return UNIQUE_BOORU_DOMAINS
-    },
   },
 
   methods: {
-    booruChangeListener(event) {
-      event.preventDefault()
-
-      const BOORU = event.target.value
-
-      this.selectedBooru = BOORU
+    async onDomainChange(DOMAIN) {
+      this.selectedBooru = DOMAIN
     },
 
     sortPostsByDate(POSTS) {
