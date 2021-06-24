086a978e9bc56b2dc84c261db2d358d61c79613a
Alejandro Akbal <37181533+AlejandroAkbal@users.noreply.github.com>
2021-06-24 20:16:13 +0200

    refactor: extract scroller functionality to component

---
+++ components/pages/posts/post/DynamicPostScroller.vue
@@ -0,0 +1,43 @@
+<template>
+  <DynamicScroller
+    :items="posts"
+    :min-item-size="1000"
+    :page-mode="true"
+    :buffer="dynamicBufferHeight"
+  >
+    <template v-slot="{ item, index, active }">
+      <!--  -->
+      <DynamicScrollerItem :item="item" :active="active" :data-index="index">
+        <!--  -->
+        <Post :key="item.id" :post-domain="postsDomain" :post-data="item" />
+      </DynamicScrollerItem>
+    </template>
+  </DynamicScroller>
+</template>
+
+<script>
+export default {
+  props: {
+    posts: {
+      type: Array,
+      required: true,
+    },
+
+    postsDomain: {
+      type: String,
+      required: true,
+    },
+
+    viewOnly: {
+      type: Boolean,
+      default: false,
+    },
+  },
+
+  computed: {
+    dynamicBufferHeight() {
+      return window.screen.availHeight * 1.5
+    },
+  },
+}
+</script>
