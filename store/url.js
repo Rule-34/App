49ef83149a61c213f5eaa8be53c82da3d4d58729
Alejandro Akbal <37181533+AlejandroAkbal@users.noreply.github.com>
2021-10-17 11:07:21 +0200

    refactor: extract functionality into helper

--- store/url.js
+++ store/url.js
@@ -1,3 +1,5 @@
+import { RouterHelper } from '~/assets/js/RouterHelper'
+
 export const getters = {
   urlDomain(state, getters, rootState, rootGetters) {
     const { domain } = rootState.route.query
@@ -33,34 +35,27 @@ export const actions = {
     const shouldReplaceHistory =
       domain === undefined && page === undefined && tags === undefined
 
+    // Get applied domain or default domain
     if (domain === undefined) {
       domain = rootGetters['booru/getActiveBooru'].domain
     }
 
+    // Get applied page or default page
     if (page === undefined) {
       page = rootGetters['booru/getPageID']
     }
 
+    // Get applied tags
     if (tags === undefined) {
       tags = rootGetters['booru/getTags']
     }
 
-    const routerLocation = {
-      query: {
-        domain,
-        page,
-      },
-    }
-
-    // Set tags
-    if (tags.length) {
-      routerLocation.query.tags = tags.join(',')
-    }
+    const ROUTE = RouterHelper.generatePostsRoute(domain, page, tags)
 
     if (shouldReplaceHistory) {
-      await this.$router.replace(routerLocation)
+      await this.$router.replace(ROUTE)
     } else {
-      await this.$router.push(routerLocation)
+      await this.$router.push(ROUTE)
     }
   },
 }
