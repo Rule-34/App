6a42fc4c747ebebd66789460ae39e2fda01552e1
Alejandro Akbal <37181533+AlejandroAkbal@users.noreply.github.com>
2022-01-06 18:44:16 +0100

    chore: add fix for axios

--- nuxt.config.js
+++ nuxt.config.js
@@ -117,6 +117,9 @@ export default {
 
   axios: {
     progress: true,
+
+    // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
+    baseURL: '/',
   },
 
   auth: {
