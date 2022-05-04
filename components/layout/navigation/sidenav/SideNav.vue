7d469e7f82e0682055e1f2d2b09fdf8d06ed5393
Alejandro Akbal <37181533+AlejandroAkbal@users.noreply.github.com>
2022-05-04 10:23:01 +0200

    style: capitalize words

--- components/layout/navigation/sidenav/SideNav.vue
+++ components/layout/navigation/sidenav/SideNav.vue
@@ -26,7 +26,7 @@
           class="absolute bottom-0 left-0 p-3 transition-colors duration-300 link"
         >
           <h1 class="text-lg font-semibold">Rule 34 App</h1>
-          <h3>Browse the most popular boorus</h3>
+          <h3>Browse the most popular Boorus</h3>
         </div>
       </div>
 
