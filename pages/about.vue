3e9db9008b7c392fcb9f4fa698f6403fd3fc344c
Alejandro Akbal <37181533+AlejandroAkbal@users.noreply.github.com>
2022-07-10 10:47:31 +0200

    chore: change about page to direct to the documentation website

--- pages/about.vue
+++ pages/about.vue
@@ -1,80 +1,38 @@
 <template>
-  <div
-    class="
-      relative
-      flex flex-col
-      items-center
-      justify-center
-      max-w-3xl
-      min-h-screen
-      px-4
-      mx-auto
-      sm:px-6
-      lg:px-8
-    "
-  >
-    <main class="p-5 material-container">
-      <img
-        src="https://cdn.statically.io/img/raw.githubusercontent.com/h=1024,w=1024,q=90/AlejandroAkbal/brand/main/src/assets/Logo_white.png"
-        alt="Akbal logo"
-        height="1024"
-        width="1024"
-        class="w-1/2 h-auto mx-auto rounded-full border-util"
+  <main class="max-w-3xl min-h-screen p-4 mx-auto sm:p-6 lg:p-8">
+    <div class="space-y-4">
+      <ContentSeparator title="About" />
+
+      <ContentContainer
+        :links="[
+          {
+            text: 'Documentation website',
+            href: 'https://docs.r34.app',
+          },
+        ]"
+        icon="bg-svg-info"
+        text="The Rule 34 App is a Progressive Web App to browse popular Rule 34 Hentai Porn for free. Without ads.
+
+        You can find more info in the documentation website."
+        title="Description"
       />
-
-      <a
-        href="https://akbal.dev/"
-        target="_blank"
-        class="
-          block
-          px-3
-          py-1
-          mx-auto
-          mt-5
-          mb-3
-          rounded-full
-          link
-          w-max
-          bg-darkGray-700
-          border-util
-        "
-      >
-        Alejandro Akbal
-      </a>
-
-      <small class="block text-center text-gray-200">
-        The software engineer that created this app, works whenever he has time
-        and will.
-      </small>
-    </main>
-
-    <footer
-      class="absolute inset-x-0 bottom-0 p-2 text-xs text-center text-gray-300"
-    >
-      v{{ app_version }}
-    </footer>
-  </div>
+    </div>
+  </main>
 </template>
 
 <script>
-import { version } from '~/package.json'
-
 export default {
-  data() {
-    return { app_version: version }
-  },
-
   head() {
     return {
-      title: 'About',
+      title: "About",
       meta: [
         {
-          hid: 'description',
-          name: 'description',
-          content: 'Information about the creator.',
-        },
-      ],
-    }
-  },
-}
+          hid: "description",
+          name: "description",
+          content: "Information about the App."
+        }
+      ]
+    };
+  }
+};
 </script>
