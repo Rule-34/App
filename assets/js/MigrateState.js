fea73b7d1b3b08a03a6a58f80f5dd861b3fcf8b5
Alejandro Akbal <37181533+AlejandroAkbal@users.noreply.github.com>
2022-09-30 19:16:18 +0200

    refactor: extract into own file

--- assets/js/MigrateState.js
+++ assets/js/MigrateState.js
@@ -1,7 +1,5 @@
 'use strict'
 
-import { cloneDeep } from 'lodash-es'
-
 /**
  * Migrate the state to new versions
  * @param {Object} state
@@ -67,44 +65,4 @@ function migrateVersion0State(state) {
   state.version = 1
 
   return state
-}
-
-export function createStateFromStore(store) {
-  const STATE = {
-    version: store.getters['getVersion'],
-
-    user: {
-      custom: {
-        boorus: store.getters['user/getCustomBoorus'],
-        tagCollections: store.getters['user/getCustomTagCollections'],
-        savedPosts: store.getters['user/getCustomSavedPosts']
-      }
-    }
-  }
-
-  return cloneDeep(STATE)
-}
-
-export function restoreStateToStore(state, store) {
-  if (!state.version) {
-    throw new Error('Invalid state')
-  }
-
-  // TODO: Think about what happens when an old state version is restored
-  store.commit('setVersion', state.version)
-
-  if (state.user.custom.boorus) {
-    store.commit('user/setCustomBoorus', state.user.custom.boorus)
-  }
-
-  if (state.user.custom.tagCollections) {
-    store.commit(
-      'user/setCustomTagCollections',
-      state.user.custom.tagCollections
-    )
-  }
-
-  if (state.user.custom.savedPosts) {
-    store.commit('user/setCustomSavedPosts', state.user.custom.savedPosts)
-  }
 }
