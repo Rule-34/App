dc201eca005fd25d88553a6b70a4d639186171f4
Alejandro Akbal <37181533+AlejandroAkbal@users.noreply.github.com>
2022-09-30 19:06:47 +0200

    refactor(State migrator): use state builder and restorer from backup

--- assets/js/MigrateState.js
+++ assets/js/MigrateState.js
@@ -66,3 +66,41 @@ function migrateVersion0State(state) {
 
   return state
 }
+
+export function createStateFromStore(store) {
+  return {
+    version: store.getters['getVersion'],
+
+    user: {
+      custom: {
+        boorus: store.getters['user/getCustomBoorus'],
+        tagCollections: store.getters['user/getCustomTagCollections'],
+        savedPosts: store.getters['user/getCustomSavedPosts']
+      }
+    }
+  }
+}
+
+export function restoreStateToStore(state, store) {
+  if (!state.version) {
+    throw new Error('Invalid state')
+  }
+
+  // TODO: Think about what happens when an old state version is restored
+  store.commit('setVersion', state.version)
+
+  if (state.user.custom.boorus) {
+    store.commit('user/setCustomBoorus', state.user.custom.boorus)
+  }
+
+  if (state.user.custom.tagCollections) {
+    store.commit(
+      'user/setCustomTagCollections',
+      state.user.custom.tagCollections
+    )
+  }
+
+  if (state.user.custom.savedPosts) {
+    store.commit('user/setCustomSavedPosts', state.user.custom.savedPosts)
+  }
+}
