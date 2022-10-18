acd2c8de29b5596cb8c00f6ee1f02afd7a6df6ba
Alejandro Akbal <37181533+AlejandroAkbal@users.noreply.github.com>
2022-10-18 18:51:14 +0200

    feat: encode url tags

--- assets/js/RouterHelper.js
+++ assets/js/RouterHelper.js
@@ -20,7 +20,7 @@ export class RouterHelper {
 
     // Fix: separate tags by commas instead of repeating `tags`, like so: `&tags=example1&tags=example2`
     if (tags != null && Array.isArray(tags) && tags.length) {
-      route.query.tags = tags.join('|')
+      route.query.tags = tags.map((tag) => encodeURI(tag)).join('|')
     }
 
     return route
