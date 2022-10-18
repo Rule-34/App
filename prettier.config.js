861b89a0768bedf829ed0036e613a7daf93f5fbf
Alejandro Akbal <37181533+AlejandroAkbal@users.noreply.github.com>
2022-10-18 18:53:25 +0200

    build: configure prettier

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  printWidth: 120,
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'none',
+  vueIndentScriptAndStyle: true,
+  singleAttributePerLine: true,
+  plugins: [require('prettier-plugin-tailwindcss')]
+}
