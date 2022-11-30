50db3410c0582de70e80ad8948c20ec43c8d8b6b
Alejandro Akbal <37181533+AlejandroAkbal@users.noreply.github.com>
2022-11-30 18:47:07 +0100

    feat(Premium): trim license input

--- components/pages/premium/PremiumLogin.vue
+++ components/pages/premium/PremiumLogin.vue
@@ -1,30 +1,30 @@
 <template>
-  <div class="material-container p-4">
-    <form @submit.prevent="userLogin">
+  <div class='material-container p-4'>
+    <form @submit.prevent='userLogin'>
       <label
-        class="mb-2 block text-lg font-medium leading-tight text-white"
-        for="license-key"
+        class='mb-2 block text-lg font-medium leading-tight text-white'
+        for='license-key'
       >
         License Key
       </label>
 
-      <div class="mt-1 flex w-full flex-row items-center gap-1">
+      <div class='mt-1 flex w-full flex-row items-center gap-1'>
         <input
-          id="license-key"
-          v-model="login.password"
-          class="focus:focus-util material-container block w-10/12 grow appearance-none border-darkGray-100 bg-darkGray-700 p-2 text-sm font-light text-gray-300 outline-none"
-          placeholder="XXXXXXXX-XXXXXXXX-XXXXXXXX-XXXXXXXX"
-          type="text"
-          name="license-key"
+          id='license-key'
+          v-model='login.password'
+          class='focus:focus-util material-container block w-10/12 grow appearance-none border-darkGray-100 bg-darkGray-700 p-2 text-sm font-light text-gray-300 outline-none'
+          name='license-key'
+          placeholder='XXXXXXXX-XXXXXXXX-XXXXXXXX-XXXXXXXX'
           required
+          type='text'
         />
 
         <!-- Action -->
         <button
-          type="submit"
-          class="focus:focus-util ml-1 appearance-none rounded-full border-0 bg-gradient-to-br from-primary-400 to-accent-400"
+          class='focus:focus-util ml-1 appearance-none rounded-full border-0 bg-gradient-to-br from-primary-400 to-accent-400'
+          type='submit'
         >
-          <chevron-right-icon class="icon h-9 w-9" />
+          <chevron-right-icon class='icon h-9 w-9' />
         </button>
       </div>
     </form>
@@ -32,28 +32,39 @@
 </template>
 
 <script>
-import { mapActions } from 'vuex'
-import { ChevronRightIcon } from 'vue-feather-icons'
+  import { mapActions } from 'vuex'
+  import { ChevronRightIcon } from 'vue-feather-icons'
 
-export default {
-  components: {
-    ChevronRightIcon
-  },
+  export default {
+    components: {
+      ChevronRightIcon
+    },
 
-  data() {
-    return {
+    data() {
+      return {
+        login: {
+          password: ''
+        }
+      }
+    },
+
+    watch: {
       login: {
-        password: ''
+        handler() {
+
+          this.login.password = this.login.password.trim()
+        },
+        
+        deep: true
+      }
+    },
+
+    methods: {
+      ...mapActions('premium', ['authenticate']),
+
+      async userLogin() {
+        await this.authenticate(this.login)
       }
     }
-  },
-
-  methods: {
-    ...mapActions('premium', ['authenticate']),
-
-    async userLogin() {
-      await this.authenticate(this.login)
-    }
   }
-}
 </script>
