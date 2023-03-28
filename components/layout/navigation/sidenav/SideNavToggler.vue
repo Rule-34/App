784799c61fb01d5301dc38ab806d4a7ea4901405
Alejandro Akbal <37181533+AlejandroAkbal@users.noreply.github.com>
2023-03-28 13:08:27 +0200

    feat(Premium): add info about PPP discount & improve accessibility

--- components/layout/navigation/sidenav/SideNavToggler.vue
+++ components/layout/navigation/sidenav/SideNavToggler.vue
@@ -1,30 +1,35 @@
 <template>
-  <button
-    aria-label='Toggle the menu'
-    class='focus-visible:focus-util flex h-12 w-12 items-center justify-center rounded-full border-0 bg-gradient-to-b from-primary-400 to-accent-400 p-4 shadow md:h-16 md:w-16'
-    title='Menu'
-    type='menu'
-    @click='toggleSideNav'
-  >
-    <MenuIcon class='icon' />
-  </button>
+	<button
+		aria-label="Toggle the menu"
+		class="focus-visible:focus-util flex h-12 w-12 items-center justify-center rounded-full border-0 bg-gradient-to-b from-primary-400 to-accent-400 p-4 shadow md:h-16 md:w-16"
+		title="Menu"
+		type="menu"
+		@click="toggleSideNav"
+	>
+		<span class="sr-only">Toggle the menu</span>
+
+		<MenuIcon
+			aria-hidden="true"
+			class="icon"
+		/>
+	</button>
 </template>
 
 <script>
-  import { mapActions } from 'vuex'
-  import { MenuIcon } from 'vue-feather-icons'
+import { mapActions } from 'vuex'
+import { MenuIcon } from 'vue-feather-icons'
 
-  export default {
-    components: {
-      MenuIcon
-    },
+export default {
+	components: {
+		MenuIcon
+	},
 
-    methods: {
-      ...mapActions('navigation', ['sideNavNavigationManager']),
+	methods: {
+		...mapActions('navigation', ['sideNavNavigationManager']),
 
-      toggleSideNav() {
-        this.sideNavNavigationManager({ operation: 'toggle' })
-      }
-    }
-  }
+		toggleSideNav() {
+			this.sideNavNavigationManager({ operation: 'toggle' })
+		}
+	}
+}
 </script>
