a2a9896415f0b533105fdd28b1e6baa4ffeca65d
Alejandro Akbal <37181533+AlejandroAkbal@users.noreply.github.com>
2023-04-14 12:13:02 +0200

    feat(Partners page): add Kurosearch

--- pages/partners.vue
+++ pages/partners.vue
@@ -1,48 +1,63 @@
 <template>
-  <main class="mx-auto min-h-screen max-w-3xl p-4 sm:p-6 lg:p-8">
-    <div class="space-y-4">
-      <ContentSeparator text="Friends of the Rule 34 App" title="Partners" />
+	<main class="mx-auto min-h-screen max-w-3xl p-4 sm:p-6 lg:p-8">
+		<div class="space-y-4">
+			<ContentSeparator
+				text="Friends of the Rule 34 App"
+				title="Partners"
+			/>
 
-      <ContentContainer
-        :links="[
-          {
-            text: 'hentaiporn.app',
-            href: 'https://hentaiporn.app/?utm_source=Rule34App&utm_medium=Partners'
-          }
-        ]"
-        icon="bg-svg-star"
-        text="Sister site of the Rule 34 App, has the best high quality Hentai."
-        title="HentaiPorn App"
-      />
+			<ContentContainer
+				:links="[
+					{
+						text: 'hentaiporn.app',
+						href: 'https://hentaiporn.app/?utm_source=Rule34App&utm_medium=Partners'
+					}
+				]"
+				icon="bg-svg-star"
+				text="Sister site of the Rule 34 App, has the best high-quality Hentai Videos and Images."
+				title="HentaiPorn App"
+			/>
 
-      <ContentContainer
-        :links="[
-          {
-            text: 'theporndude.com',
-            href: 'https://theporndude.com'
-          }
-        ]"
-        icon="bg-svg-star"
-        text="The Porn Dude lists the world's best porn sites."
-        title="ThePornDude"
-      />
-    </div>
-  </main>
+			<ContentContainer
+				:links="[
+					{
+						text: 'kurosearch.com',
+						href: 'https://kurosearch.com/?utm_source=Rule34App&utm_medium=Partners'
+					}
+				]"
+				icon="bg-svg-star"
+				text="Friend of the Rule 34 App, has a slick interface and a lot of content."
+				title="Kurosearch"
+			/>
+
+			<ContentContainer
+				:links="[
+					{
+						text: 'theporndude.com',
+						href: 'https://theporndude.com'
+					}
+				]"
+				icon="bg-svg-star"
+				text="The Porn Dude lists the world's best porn sites."
+				title="ThePornDude"
+			/>
+		</div>
+	</main>
 </template>
 
 <script>
 export default {
-  head() {
-    return {
-      title: 'Partners',
-      meta: [
-        {
-          hid: 'description',
-          name: 'description',
-          content: 'Friends of the Rule 34 App.'
-        }
-      ]
-    }
-  }
+	head() {
+		return {
+			title: 'Partners',
+			meta: [
+				{
+					hid: 'description',
+					name: 'description',
+					content: 'Friends of the Rule 34 App.'
+				}
+			]
+		}
+	}
 }
 </script>
