5cad8d0860cc46b20c9cea1c9d8f429127c7f566
Alejandro Akbal <37181533+AlejandroAkbal@users.noreply.github.com>
2023-04-15 14:46:15 +0200

    feat: create scroll to top button

--- components/layout/navigation/sidenav/SideNavToggler.vue
+++ components/layout/navigation/sidenav/SideNavToggler.vue
@@ -3,7 +3,7 @@
 		aria-label="Toggle the menu"
 		class="focus-visible:focus-util flex h-12 w-12 items-center justify-center rounded-full border-0 bg-gradient-to-b from-primary-400 to-accent-400 p-4 shadow md:h-16 md:w-16"
 		title="Menu"
-		type="menu"
+		type="button"
 		@click="toggleSideNav"
 	>
 		<span class="sr-only">Toggle the menu</span>
