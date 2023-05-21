ecbc7a7a8d283d34458404d843eea327120be438
Alejandro Akbal <37181533+AlejandroAkbal@users.noreply.github.com>
2023-05-21 21:05:43 +0200

    feat: create pagination component

---
+++ components/pages/posts/navigation/PostsPagination.vue
@@ -0,0 +1,22 @@
+<script setup>
+  import { vIntersectionObserver } from '@vueuse/components'
+
+  const emit = defineEmits(['loadNextPage'])
+
+  function onIntersectionObserver([{ isIntersecting }]) {
+    if (!isIntersecting) {
+      return
+    }
+
+    emit('loadNextPage')
+  }
+</script>
+
+<template>
+  <div
+    v-intersection-observer="[onIntersectionObserver, { rootMargin: '650px' }]"
+    class="flex animate-pulse items-center justify-center rounded-md px-4 py-2 text-sm font-medium text-base-content ring-1 ring-base-0/20"
+  >
+    Loading next page&hellip;
+  </div>
+</template>
