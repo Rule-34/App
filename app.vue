cb646dc7e03332e0890963aa1e5d17455889bce1
Alejandro Akbal <37181533+AlejandroAkbal@users.noreply.github.com>
2023-05-23 13:42:45 +0200

    fix(Headers): move to useHead

--- app.vue
+++ app.vue
@@ -1,4 +1,6 @@
 <script setup>
+  const config = useRuntimeConfig()
+
   useHead({
     htmlAttrs: {
       lang: 'en',
@@ -11,7 +13,32 @@
 
     titleTemplate: (titleChunk) => {
       return titleChunk ? `${titleChunk} | Rule 34 App` : 'Rule 34 App'
-    }
+    },
+
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+
+      {
+        name: 'monetization',
+        content: config.public.MONETIZATION_URI
+      },
+
+      // TODO: Improve description
+      {
+        name: 'description',
+        content: 'Stream and download images, GIFs and videos hentai. For free!'
+      }
+    ],
+
+    link: [
+      // Font
+      {
+        rel: 'stylesheet',
+        type: 'text/css',
+        href: 'https://rsms.me/inter/inter.css'
+      }
+    ]
   })
 </script>
 
