5d00605a195056cc35f0556c1c521fbb0d11086d
Alejandro Akbal <37181533+AlejandroAkbal@users.noreply.github.com>
2023-06-01 10:02:55 +0200

    feat: use custom query string parser

---
+++ app/router.options.ts
@@ -0,0 +1,10 @@
+import type { RouterConfig } from '@nuxt/schema'
+import qs from 'qs'
+
+// https://router.vuejs.org/api/interfaces/routeroptions.html
+export default <RouterConfig>{
+  // Fix router query parser - https://github.com/vuejs/vue-router/issues/1259#issuecomment-1571553624
+  parseQuery: qs.parse,
+
+  stringifyQuery: qs.stringify
+}
