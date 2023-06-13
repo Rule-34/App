98f630ef6f070ee1e466f116d372d73c26aef9c0
Alejandro Akbal <37181533+AlejandroAkbal@users.noreply.github.com>
2023-06-14 00:28:41 +0200

    feat: generate sitemap with most popular tags

---
+++ server/api/_sitemap-urls.ts
@@ -0,0 +1,64 @@
+const config = useRuntimeConfig()
+
+/**
+ * @see https://github.com/harlan-zw/nuxt-simple-sitemap#handling-dynamic-urls
+ */
+export default cachedEventHandler(
+  async () => {
+    // TODO: Fetch more data
+    const popularSiteSearchKeywords = await getPopularSiteSearchKeywordsFromMatomoApi()
+
+    return popularSiteSearchKeywords.map((keyword) => ({
+      loc: `/posts?tags=${keyword.label}`,
+      changefreq: 'always',
+      priority: 0.8
+    }))
+  },
+  {
+    name: 'sitemap-dynamic-urls',
+    maxAge: 60 * 10 // cache URLs for 10 minutes
+  }
+)
+
+interface MatomoResponse {
+  label: string
+  nb_visits: number
+  nb_hits: number
+  sum_time_spent: number
+  exit_nb_visits: number
+  nb_pages_per_search: number
+  avg_time_on_page: number
+  bounce_rate: string
+  exit_rate: string
+  segment: string
+}
+
+interface MatomoErrorResponse {
+  result: 'error'
+  message: string
+}
+
+async function getPopularSiteSearchKeywordsFromMatomoApi() {
+  const baseUrl = `https://${config.public.MATOMO_HOST}/`
+  const apiToken = config.MATOMO_API_KEY
+
+  const siteId = config.public.MATOMO_SITE_ID
+  const period = 'month'
+  const date = 'lastMonth'
+
+  const response = await fetch(
+    `${baseUrl}?module=API&method=Actions.getSiteSearchKeywords&idSite=${siteId}&period=${period}&date=${date}&format=JSON&token_auth=${apiToken}`
+  )
+
+  if (!response.ok) {
+    throw new Error(response.statusText)
+  }
+
+  const data: MatomoResponse[] | MatomoErrorResponse = await response.json()
+
+  if (data.result === 'error') {
+    throw new Error(data.message)
+  }
+
+  return data
+}
