72563c82371376d0259c316c0041866241461681
Alejandro Akbal <37181533+AlejandroAkbal@users.noreply.github.com>
2023-06-15 09:46:17 +0200

    feat(layout): change max width

--- components/layout/navigation/Navbar.vue
+++ components/layout/navigation/Navbar.vue
@@ -10,7 +10,7 @@
     <!-- -->
 
     <!-- Container -->
-    <div class="container mx-auto sm:px-6 lg:px-8">
+    <div class="mx-auto max-w-3xl sm:px-6 lg:px-8">
       <!-- -->
 
       <!-- Navbar -->
