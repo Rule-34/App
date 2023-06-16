1f47ebf928bec3579fb75f6dd10170d1955c3865
Alejandro Akbal <37181533+AlejandroAkbal@users.noreply.github.com>
2023-06-16 09:59:19 +0200

    feat(layout): change max width

--- pages/settings.vue
+++ pages/settings.vue
@@ -25,7 +25,7 @@
 </script>
 
 <template>
-  <main class="container mx-auto flex-1 px-4 sm:px-6 lg:px-8">
+  <main class="container mx-auto max-w-3xl flex-1 px-4 sm:px-6 lg:px-8">
     <div class="relative flex h-full flex-col space-y-12">
       <!-- Header -->
       <ContentSeparator
