71cc59a68b5854818b9fba71692cacde4153406d
Alejandro Akbal <37181533+AlejandroAkbal@users.noreply.github.com>
2023-06-20 15:37:07 +0200

    feat: create WIP pages

---
+++ pages/premium/additional-boorus.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+  useSeoMeta({
+    title: 'Additional Boorus'
+  })
+
+  definePageMeta({ middleware: 'auth' })
+</script>
+
+<template>
+  <main class="container mx-auto max-w-3xl flex-1 px-4 py-4 sm:px-6 lg:px-8">
+    <PageHeader>
+      <template #title>Additional Boorus</template>
+      <template #text> Browse posts from other Boorus </template>
+    </PageHeader>
+
+    <!-- TODO -->
+  </main>
+</template>
