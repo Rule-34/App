dafc5c623d25c44fdcc523c5eb434f6de411337e
Alejandro Akbal <37181533+AlejandroAkbal@users.noreply.github.com>
2023-06-20 15:37:07 +0200

    feat: create WIP pages

---
+++ pages/premium/tag-collections.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+  useSeoMeta({
+    title: 'Tag collections'
+  })
+
+  definePageMeta({ middleware: 'auth' })
+</script>
+
+<template>
+  <main class="container mx-auto max-w-3xl flex-1 px-4 py-4 sm:px-6 lg:px-8">
+    <PageHeader>
+      <template #title>Tag collections</template>
+      <template #text>Create lists of tags to quickly search or filter posts</template>
+    </PageHeader>
+
+    <!-- TODO -->
+  </main>
+</template>
