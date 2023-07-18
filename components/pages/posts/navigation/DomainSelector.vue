6abcd948b954c1db52d46d53988c1290ab4eba40
Alejandro Akbal <37181533+AlejandroAkbal@users.noreply.github.com>
2023-07-18 11:16:28 +0200

    feat(domain selector): add compact mode

--- components/pages/posts/navigation/DomainSelector.vue
+++ components/pages/posts/navigation/DomainSelector.vue
@@ -7,6 +7,8 @@
   interface DomainSelectorProps {
     boorus: Domain[]
     modelValue: Domain
+
+    compact?: boolean
   }
 
   const props = defineProps<DomainSelectorProps>()
@@ -26,7 +28,8 @@
     >
       <!-- Select -->
       <ListboxButton
-        class="hover:hover-text-util focus-visible:focus-outline-util hover:hover-bg-util relative w-56 cursor-default rounded-md py-1.5 pl-3 pr-10 text-left ring-1 ring-inset ring-base-0/20 sm:text-sm sm:leading-6"
+        :class="[props.compact ? 'w-auto !rounded-full !p-2.5' : 'w-56']"
+        class="hover:hover-text-util focus-visible:focus-outline-util hover:hover-bg-util relative cursor-default rounded-md py-1.5 pl-3 pr-10 text-left ring-1 ring-inset ring-base-0/20 sm:text-sm sm:leading-6"
       >
         <span class="flex items-center">
           <img
@@ -40,11 +43,18 @@
 
           <span class="sr-only"> Website to browse: </span>
 
-          <span class="ml-3 block truncate">{{ props.modelValue.domain }}</span>
+          <span
+            v-if="!props.compact"
+            class="ml-3 block truncate"
+            >{{ props.modelValue.domain }}</span
+          >
         </span>
 
         <!-- Chevron -->
-        <span class="pointer-events-none absolute inset-y-0 right-0 ml-3 flex items-center pr-2">
+        <span
+          v-if="!props.compact"
+          class="pointer-events-none absolute inset-y-0 right-0 ml-3 flex items-center pr-2"
+        >
           <ChevronUpDownIcon class="h-5 w-5" />
         </span>
       </ListboxButton>
