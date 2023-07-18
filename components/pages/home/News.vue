e1e21e8557150276ac94ffc7ead67c5e14cbd5c7
Alejandro Akbal <37181533+AlejandroAkbal@users.noreply.github.com>
2023-07-18 18:26:11 +0200

    feat(home page): add news

---
+++ components/pages/home/News.vue
@@ -0,0 +1,52 @@
+<script lang="ts" setup>
+  const newsList = [
+    {
+      name: 'Install the App',
+      text: 'Learn how to install the Rule 34 App on your device, in 3 simple steps',
+      links: [
+        {
+          text: 'Install the App',
+          href: 'https://www.installpwa.com/from/r34.app',
+          target: '_blank'
+        }
+      ]
+    },
+    {
+      name: 'Social media Bots',
+      text: 'Enjoy the best hentai anywhere and anytime',
+      links: [
+        {
+          text: 'Twitter',
+          href: 'https://twitter.com/Rule34_Pokebot',
+          target: '_blank'
+        },
+        {
+          text: 'Telegram',
+          href: 'https://t.me/Rule_34_Hentai',
+          target: '_blank'
+        },
+        {
+          text: 'Mastodon',
+          href: 'https://mastodon.social/@r34',
+          target: '_blank'
+        }
+      ]
+    }
+  ]
+</script>
+
+<template>
+  <ol class="space-y-4">
+    <li
+      v-for="news in newsList"
+      :key="news.name"
+    >
+      <ContentContainer
+        :links="news.links"
+        :text="news.text"
+        as="h3"
+        :title="news.name"
+      />
+    </li>
+  </ol>
+</template>
