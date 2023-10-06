9e300f9dfe0db2b1b1a4397cef203f577e53f7e7
Alejandro Akbal <37181533+AlejandroAkbal@users.noreply.github.com>
2023-10-06 12:21:37 +0100

    feat(premium page): add rating

--- components/layout/navigation/Navbar.vue
+++ components/layout/navigation/Navbar.vue
@@ -1,71 +1,79 @@
 <script setup>
-  import { Bars3Icon, XMarkIcon } from '@heroicons/vue/24/outline'
+import { Bars3Icon, XMarkIcon } from '@heroicons/vue/24/outline'
 
-  const route = useRoute()
+const route = useRoute()
 
-  const { value: isMenuActive, toggle: toggleMenu } = useMenu()
+const { value: isMenuActive, toggle: toggleMenu } = useMenu()
 
-  const isHomePage = computed(() => route.path === '/')
+const isPageWithLogoDisabled = computed(() => {
+	switch (route.path) {
+		case '/':
+			return true
+
+		default:
+			return false
+	}
+})
 </script>
 
 <template>
-  <nav>
-    <!-- -->
+	<nav>
+		<!-- -->
 
-    <!-- Container -->
-    <div class="container mx-auto max-w-3xl sm:px-6 lg:px-8">
-      <!-- -->
+		<!-- Container -->
+		<div class='container mx-auto max-w-3xl sm:px-6 lg:px-8'>
+			<!-- -->
 
-      <!-- Navbar -->
-      <div class="relative flex h-14 items-center justify-between">
-        <!-- -->
+			<!-- Navbar -->
+			<div class='relative flex h-14 items-center justify-between'>
+				<!-- -->
 
-        <!-- Right side: Menu button -->
-        <div class="absolute inset-y-0 left-0 flex items-center pl-2">
-          <button
-            class="hover:hover-text-util focus-visible:focus-outline-util hover:hover-bg-util inline-flex items-center justify-center rounded-md p-2 text-base-content-highlight focus-visible:ring-inset"
-            type="button"
-            @click="toggleMenu()"
-          >
-            <span class="sr-only">Open main menu</span>
+				<!-- Right side: Menu button -->
+				<div class='absolute inset-y-0 left-0 flex items-center pl-2'>
+					<button
+						class='hover:hover-text-util focus-visible:focus-outline-util hover:hover-bg-util inline-flex items-center justify-center rounded-md p-2 text-base-content-highlight focus-visible:ring-inset'
+						type='button'
+						@click='toggleMenu()'
+					>
+						<span class='sr-only'>Open main menu</span>
 
-            <Bars3Icon
-              v-if="!isMenuActive"
-              class="block h-6 w-6"
-            />
+						<Bars3Icon
+							v-if='!isMenuActive'
+							class='block h-6 w-6'
+						/>
 
-            <XMarkIcon
-              v-else
-              class="block h-6 w-6"
-            />
-          </button>
-        </div>
+						<XMarkIcon
+							v-else
+							class='block h-6 w-6'
+						/>
+					</button>
+				</div>
 
-        <!-- Center: Logo -->
-        <div class="flex flex-1 items-center justify-center">
-          <NuxtLink
-            v-if="!isHomePage"
-            class="focus-visible:focus-outline-util flex-shrink-0"
-            to="/"
-          >
-            <img
-              alt="Icon"
-              class="flip-vertical-fwd h-6 w-6 text-base-content-highlight"
-              height="16"
-              loading="eager"
-              src="/icon.svg"
-              width="16"
-            />
-          </NuxtLink>
-        </div>
+				<!-- Center: Logo -->
+				<div class='flex flex-1 items-center justify-center'>
+					<NuxtLink
+						v-if='!isPageWithLogoDisabled'
+						class='focus-visible:focus-outline-util flex-shrink-0'
+						to='/'
+					>
+						<img
+							alt='Icon'
+							class='flip-vertical-fwd h-6 w-6 text-base-content-highlight'
+							height='16'
+							loading='eager'
+							src='/icon.svg'
+							width='16'
+						/>
+					</NuxtLink>
+				</div>
 
-        <!-- Right side: Actions -->
-        <TeleportTarget
-          id="navbar-actions"
-          class="absolute inset-y-0 right-0 flex flex-row-reverse items-center pr-2"
-        >
-        </TeleportTarget>
-      </div>
-    </div>
-  </nav>
+				<!-- Right side: Actions -->
+				<TeleportTarget
+					id='navbar-actions'
+					class='absolute inset-y-0 right-0 flex flex-row-reverse items-center pr-2'
+				>
+				</TeleportTarget>
+			</div>
+		</div>
+	</nav>
 </template>
--- pages/premium/backup.vue
+++ pages/premium/backup.vue
@@ -1,162 +1,163 @@
-<script lang="ts" setup>
-  import { ArrowDownTrayIcon, ArrowUturnLeftIcon } from '@heroicons/vue/24/solid'
-  import PageHeader from '~/components/layout/PageHeader.vue'
-  import { toast } from 'vue-sonner'
-  import type { ISavedPost } from '~/store/SavedPosts'
-  import type { ITagCollection } from 'assets/js/tagCollection.dto'
+<script lang='ts' setup>
+import { ArrowDownTrayIcon, ArrowUturnLeftIcon } from '@heroicons/vue/24/solid'
+import PageHeader from '~/components/layout/PageHeader.vue'
+import { toast } from 'vue-sonner'
+import type { ISavedPost } from '~/store/SavedPosts'
+import type { ITagCollection } from 'assets/js/tagCollection.dto'
 
-  interface IBackupState {
-    version: number
+interface IBackupState {
+	version: number
 
-    saved_posts: ISavedPost[]
-    tag_collections: ITagCollection[]
+	saved_posts: ISavedPost[]
+	tag_collections: ITagCollection[]
 
-    settings: undefined
-  }
+	settings: undefined
+}
 
-  const fileInputElement = ref<HTMLInputElement | null>(null)
+const fileInputElement = ref<HTMLInputElement | null>(null)
 
-  function downloadBlob(blob: Blob, filename: string) {
-    const objectURL = window.URL.createObjectURL(blob)
+function downloadBlob(blob: Blob, filename: string) {
+	const objectURL = window.URL.createObjectURL(blob)
 
-    // Create anchor element
-    const anchorElement = document.createElement('a')
+	// Create anchor element
+	const anchorElement = document.createElement('a')
 
-    anchorElement.href = objectURL
-    anchorElement.target = '_blank'
-    anchorElement.download = filename
-    anchorElement.style.display = 'none'
+	anchorElement.href = objectURL
+	anchorElement.target = '_blank'
+	anchorElement.download = filename
+	anchorElement.style.display = 'none'
 
-    // Download
-    anchorElement.click()
+	// Download
+	anchorElement.click()
 
-    // Clean up
-    anchorElement.remove()
-    window.URL.revokeObjectURL(objectURL)
-  }
+	// Clean up
+	anchorElement.remove()
+	window.URL.revokeObjectURL(objectURL)
+}
 
-  function createBackup() {
-    const currentDateString = new Date()
-      .toLocaleString([], {
-        timeZone: 'UTC',
-        hour12: false,
-        second: '2-digit',
-        minute: '2-digit',
-        hour: '2-digit',
-        day: '2-digit',
-        month: '2-digit',
-        year: 'numeric'
-      })
-      .replaceAll(', ', '_')
-      .replaceAll('/', '-')
-      .replaceAll(':', '-')
+function createBackup() {
+	const currentDateString = new Date()
+		.toLocaleString([], {
+			timeZone: 'UTC',
+			hour12: false,
+			second: '2-digit',
+			minute: '2-digit',
+			hour: '2-digit',
+			day: '2-digit',
+			month: '2-digit',
+			year: 'numeric'
+		})
+		.replaceAll(', ', '_')
+		.replaceAll('/', '-')
+		.replaceAll(':', '-')
 
-    // TODO: Do versioning, or maybe not?
-    const STATE: IBackupState = {
-      version: 1,
+	// TODO: Do versioning, or maybe not?
+	const STATE: IBackupState = {
+		version: 1,
 
-      saved_posts: [],
-      tag_collections: [],
+		saved_posts: [],
+		tag_collections: [],
 
-      settings: undefined
-    }
+		settings: undefined
+	}
 
-    const blob = new Blob([JSON.stringify(STATE)], { type: 'application/json' })
+	const blob = new Blob([JSON.stringify(STATE)], { type: 'application/json' })
 
-    const fileName = `R34App_${currentDateString}_Backup.json`
+	const fileName = `R34App_${ currentDateString }_Backup.json`
 
-    downloadBlob(blob, fileName)
-  }
+	downloadBlob(blob, fileName)
+}
 
-  async function restoreBackup() {
-    if (!fileInputElement.value) {
-      toast.error('No file selected')
-      return
-    }
+async function restoreBackup() {
+	if (!fileInputElement.value) {
+		toast.error('No file selected')
+		return
+	}
 
-    const file = fileInputElement.value.files?.[0]
+	const file = fileInputElement.value.files?.[0]
 
-    if (!file) {
-      toast.error('No file selected')
-      return
-    }
+	if (!file) {
+		toast.error('No file selected')
+		return
+	}
 
-    const backupData: IBackupState = JSON.parse(await file.text())
+	const backupData: IBackupState = JSON.parse(await file.text())
 
-    if (backupData.version !== 1) {
-      toast.error('Invalid backup file')
-      return
-    }
+	if (backupData.version !== 1) {
+		toast.error('Invalid backup file')
+		return
+	}
 
-    if (backupData.saved_posts) {
-      // TODO
-    }
+	if (backupData.saved_posts) {
+		// TODO
+	}
 
-    if (backupData.tag_collections) {
-      // TODO
-    }
+	if (backupData.tag_collections) {
+		// TODO
+	}
 
-    if (backupData.settings) {
-      // TODO
-    }
+	if (backupData.settings) {
+		// TODO
+	}
 
-    toast.success('Backup restored')
-  }
+	toast.success('Backup restored')
+}
 
-  useSeoMeta({
-    title: 'Backup'
-  })
+useSeoMeta({
+	title: 'Backup'
+})
 
-  definePageMeta({ middleware: 'auth' })
+definePageMeta({ middleware: 'auth' })
 </script>
 
 <template>
-  <main class="container mx-auto max-w-3xl flex-1 px-4 py-4 sm:px-6 lg:px-8">
-    <!-- -->
+	<main class='container mx-auto max-w-3xl flex-1 px-4 py-4 sm:px-6 lg:px-8'>
+		<!-- -->
 
-    <PageHeader>
-      <template #title>Backup & Restore</template>
-      <template #text>
-        <div class="text-sm">
-          Backup your saved posts, tag collections and settings.
+		<PageHeader>
+			<template #title>Backup & Restore</template>
+			<template #text>
+				<div class='text-sm'>
+					Backup your saved posts, tag collections and settings.
 
-          <span class="italic"> This is a manual process. Remember to backup your data regularly. </span>
-        </div>
-      </template>
-    </PageHeader>
 
-    <section class="mt-8 flex justify-around">
-      <!-- -->
+					<span class='italic block mt-3'> This is a manual process. Remember to backup your data regularly. </span>
+				</div>
+			</template>
+		</PageHeader>
 
-      <!-- Backup -->
-      <button
-        class="focus-visible:focus-outline-util hover:hover-text-util hover:hover-bg-util inline-flex min-w-[5rem] flex-col items-center gap-1.5 rounded-md p-2"
-        type="button"
-        @click="createBackup"
-      >
-        <ArrowDownTrayIcon class="h-6 w-6" />
+		<section class='mt-8 flex justify-around'>
+			<!-- -->
 
-        Backup
-      </button>
+			<!-- Backup -->
+			<button
+				class='focus-visible:focus-outline-util hover:hover-text-util hover:hover-bg-util inline-flex min-w-[5rem] flex-col items-center gap-1.5 rounded-md p-2'
+				type='button'
+				@click='createBackup'
+			>
+				<ArrowDownTrayIcon class='h-6 w-6' />
 
-      <!-- Restore -->
-      <input
-        ref="fileInputElement"
-        accept="application/json"
-        style="display: none"
-        type="file"
-        @change="restoreBackup"
-      />
+				Backup
+			</button>
 
-      <button
-        class="focus-visible:focus-outline-util hover:hover-text-util hover:hover-bg-util inline-flex min-w-[5rem] flex-col items-center gap-1.5 rounded-md p-2"
-        type="button"
-        @click="$refs.fileInputElement.click()"
-      >
-        <ArrowUturnLeftIcon class="h-6 w-6" />
+			<!-- Restore -->
+			<input
+				ref='fileInputElement'
+				accept='application/json'
+				style='display: none'
+				type='file'
+				@change='restoreBackup'
+			/>
 
-        Restore
-      </button>
-    </section>
-  </main>
+			<button
+				class='focus-visible:focus-outline-util hover:hover-text-util hover:hover-bg-util inline-flex min-w-[5rem] flex-col items-center gap-1.5 rounded-md p-2'
+				type='button'
+				@click='$refs.fileInputElement.click()'
+			>
+				<ArrowUturnLeftIcon class='h-6 w-6' />
+
+				Restore
+			</button>
+		</section>
+	</main>
 </template>
