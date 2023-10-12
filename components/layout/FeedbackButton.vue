a565536194bb45a3ec722bc58f62d229f05f1f1f
Alejandro Akbal <37181533+AlejandroAkbal@users.noreply.github.com>
2023-10-12 16:51:41 +0100

    Revert "chore(feedback button): tweak"
    This reverts commit f3fdf507a2e2c245494e93539edb5d95621c24af.

--- components/layout/FeedbackButton.vue
+++ components/layout/FeedbackButton.vue
@@ -3,10 +3,10 @@
 </script>
 
 <template>
-  <div class="fixed bottom-1/2 right-0">
+  <div class="fixed bottom-1/2 right-2.5">
     <div class="flex flex-col items-center justify-center gap-2">
       <NuxtLink
-        class="focus-visible:focus-outline-util hover:hover-text-util hover:hover-bg-util flex items-center justify-center gap-2 rounded-r-md bg-base-1000/60 px-1 py-2 shadow-lg ring-1 ring-base-0/10 backdrop-blur"
+        class="focus-visible:focus-outline-util hover:hover-text-util hover:hover-bg-util flex items-center justify-center gap-2 rounded-r-md bg-base-1000/60 px-0.5 py-2 shadow-lg ring-1 ring-base-0/10 backdrop-blur"
         style="writing-mode: vertical-lr; transform: rotate(180deg)"
         target="_blank"
         to="https://forms.gle/sghxKkEgaGVpp81b9"
