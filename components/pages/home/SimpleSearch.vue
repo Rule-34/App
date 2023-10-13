08101a204cdacd072b3c25f0b30f4c637ba145d5
Alejandro Akbal <37181533+AlejandroAkbal@users.noreply.github.com>
2023-10-13 18:37:00 +0100

    fix: positioning issue

--- components/pages/home/SimpleSearch.vue
+++ components/pages/home/SimpleSearch.vue
@@ -1,141 +1,142 @@
-<script lang="ts" setup>
-  import { Combobox, ComboboxButton, ComboboxInput, ComboboxOption, ComboboxOptions } from '@headlessui/vue'
-  import { CheckIcon, ChevronUpDownIcon, MagnifyingGlassIcon } from '@heroicons/vue/20/solid'
-  import { watchDebounced } from '@vueuse/core'
-  import { abbreviateNumber } from 'js-abbreviation-number'
-  import Tag from 'assets/js/tag.dto'
+<script lang='ts' setup>
+import { Combobox, ComboboxButton, ComboboxInput, ComboboxOption, ComboboxOptions } from '@headlessui/vue'
+import { CheckIcon, ChevronUpDownIcon, MagnifyingGlassIcon } from '@heroicons/vue/20/solid'
+import { watchDebounced } from '@vueuse/core'
+import { abbreviateNumber } from 'js-abbreviation-number'
+import Tag from 'assets/js/tag.dto'
 
-  const props = defineProps<{
-    tagResults: Tag[]
-  }>()
+const props = defineProps<{
+  tagResults: Tag[]
+}>()
 
-  const emit = defineEmits<{
-    searchTag: [tag: string]
+const emit = defineEmits<{
+  searchTag: [tag: string]
 
-    submit: [payload: string | undefined]
-  }>()
+  submit: [payload: string | undefined]
+}>()
 
-  const selectedTag = ref<Tag | undefined>()
+const selectedTag = ref<Tag | undefined>()
 
-  watch(selectedTag, () => onSubmitted())
+watch(selectedTag, () => onSubmitted())
 
-  const searchQuery = ref('')
+const searchQuery = ref('')
 
-  // Change event
-  function onComboboxInputChange(event: InputEvent) {
-    let value = (event.target as HTMLInputElement).value
+// Change event
+function onComboboxInputChange(event: InputEvent) {
+  let value = (event.target as HTMLInputElement).value
 
-    value = value.trim()
+  value = value.trim()
 
-    // Replace empty spaces with underscores
-    value = value.replace(/\s/g, '_')
+  // Replace empty spaces with underscores
+  value = value.replace(/\s/g, '_')
 
-    searchQuery.value = value
+  searchQuery.value = value
+}
+
+watchDebounced(searchQuery, (value) => onSearchChange(value), { debounce: 350 })
+
+function onSearchChange(tag: string) {
+  tag = tag.trim()
+
+  if (!tag || tag === '') {
+    return
   }
 
-  watchDebounced(searchQuery, (value) => onSearchChange(value), { debounce: 350 })
+  emit('searchTag', tag)
+}
 
-  function onSearchChange(tag: string) {
-    tag = tag.trim()
+const customTagFromQuery = computed(() => {
+  let tag = searchQuery.value
 
-    if (!tag || tag === '') {
-      return
-    }
-
-    emit('searchTag', tag)
+  if (!tag || tag === '') {
+    return null
   }
 
-  const customTagFromQuery = computed(() => {
-    let tag = searchQuery.value
-
-    if (!tag || tag === '') {
-      return null
-    }
-
-    // If the tag is already in tagResults, return null
-    if (props.tagResults.some((tagResult) => tagResult.name === tag)) {
-      return null
-    }
-
-    return {
-      name: tag,
-      count: null
-    }
-  })
-
-  function onSubmitted() {
-    emit('submit', selectedTag.value?.name)
+  // If the tag is already in tagResults, return null
+  if (props.tagResults.some((tagResult) => tagResult.name === tag)) {
+    return null
   }
+
+  return {
+    name: tag,
+    count: null
+  }
+})
+
+function onSubmitted() {
+  emit('submit', selectedTag.value?.name)
+}
 </script>
 
 <template>
   <!--  Search & Actions -->
   <section>
     <!-- Search -->
-    <Combobox v-model="selectedTag">
-      <div class="group relative">
+    <!-- TODO: Use Float to fix transparency issues-->
+    <Combobox v-model='selectedTag'>
+      <div class='group relative'>
         <!-- Icon -->
-        <div class="group pointer-events-none absolute inset-y-0 left-0 flex items-center rounded-l-md px-2">
-          <MagnifyingGlassIcon class="h-5 w-5 text-base-content group-hover:text-base-content-hover" />
+        <div class='group pointer-events-none absolute inset-y-0 left-0 flex items-center rounded-l-md px-2'>
+          <MagnifyingGlassIcon class='h-5 w-5 text-base-content group-hover:text-base-content-hover' />
         </div>
 
         <!-- Input -->
         <ComboboxInput
-          :displayValue="(tag) => tag?.name"
-          class="focus-visible:focus-outline-util hover:hover-bg-util hover:hover-text-util w-full rounded-full border-0 bg-base-1000 px-9 py-2 text-base-content-highlight ring-1 ring-inset ring-base-0/20 sm:text-sm"
-          @change="onComboboxInputChange"
+          :displayValue='(tag) => tag?.name'
+          class='focus-visible:focus-outline-util hover:hover-bg-util hover:hover-text-util w-full rounded-full border-0 bg-base-1000 px-9 py-2 text-base-content-highlight ring-1 ring-inset ring-base-0/20 sm:text-sm'
+          @change='onComboboxInputChange'
         />
 
         <!-- Button -->
-        <ComboboxButton class="absolute inset-y-0 right-0 flex items-center rounded-r-md px-2">
-          <ChevronUpDownIcon class="h-5 w-5 text-base-content group-hover:text-base-content-hover" />
+        <ComboboxButton class='absolute inset-y-0 right-0 flex items-center rounded-r-md px-2'>
+          <ChevronUpDownIcon class='h-5 w-5 text-base-content group-hover:text-base-content-hover' />
         </ComboboxButton>
 
         <!-- Options -->
         <ComboboxOptions
-          class="absolute z-10 mt-2 max-h-72 w-full overflow-auto rounded-md bg-base-1000 py-1 text-base ring-1 ring-base-0/20 sm:text-sm"
+          class='absolute z-20 mt-2 max-h-72 w-full overflow-auto rounded-md bg-base-1000 py-1 text-base ring-1 ring-base-0/20 sm:text-sm'
         >
           <!-- Custom option -->
           <!-- TODO: History based -->
           <ComboboxOption
-            v-if="customTagFromQuery"
-            v-slot="{ active, selected }"
-            :value="customTagFromQuery"
+            v-if='customTagFromQuery'
+            v-slot='{ active, selected }'
+            :value='customTagFromQuery'
           >
             <div
               :class="[active ? 'bg-base-0/20 text-base-content-highlight' : 'text-base-content']"
-              class="relative cursor-default select-none py-2 pl-8"
+              class='relative cursor-default select-none py-2 pl-8'
             >
               <span :class="['block truncate', selected && 'font-semibold']">
                 Create “{{ customTagFromQuery.name }}” tag
               </span>
 
               <span
-                v-if="selected"
-                class="absolute inset-y-0 left-0 flex items-center pl-1.5 text-base-content-highlight"
+                v-if='selected'
+                class='absolute inset-y-0 left-0 flex items-center pl-1.5 text-base-content-highlight'
               >
-                <CheckIcon class="h-5 w-5" />
+                <CheckIcon class='h-5 w-5' />
               </span>
             </div>
           </ComboboxOption>
 
           <!-- Options -->
           <ComboboxOption
-            v-for="tag in tagResults"
-            :key="tag.name"
-            v-slot="{ active, selected }"
-            :value="tag"
+            v-for='tag in tagResults'
+            :key='tag.name'
+            v-slot='{ active, selected }'
+            :value='tag'
           >
             <div
               :class="[active ? 'bg-base-0/20 text-base-content-highlight' : 'text-base-content']"
-              class="relative cursor-default select-none py-2 pl-8 pr-12"
+              class='relative cursor-default select-none py-2 pl-8 pr-12'
             >
               <!-- Check icon -->
               <span
-                v-if="selected"
-                class="absolute inset-y-0 left-0 flex items-center pl-1.5 text-base-content-highlight"
+                v-if='selected'
+                class='absolute inset-y-0 left-0 flex items-center pl-1.5 text-base-content-highlight'
               >
-                <CheckIcon class="h-5 w-5" />
+                <CheckIcon class='h-5 w-5' />
               </span>
 
               <!-- Tag -->
@@ -144,8 +145,8 @@
               </span>
 
               <!-- Tag count -->
-              <div class="absolute inset-y-0 right-0 flex items-center gap-2 pr-4">
-                <span v-if="tag.count">
+              <div class='absolute inset-y-0 right-0 flex items-center gap-2 pr-4'>
+                <span v-if='tag.count'>
                   {{ abbreviateNumber(tag.count, 0) }}
                 </span>
               </div>
