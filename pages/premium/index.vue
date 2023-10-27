484a62e0feca70a53cd0d8e0ba81d4cd857ced27
Alejandro Akbal <37181533+AlejandroAkbal@users.noreply.github.com>
2023-10-27 12:36:20 +0100

    fix: use half star

--- pages/premium/index.vue
+++ pages/premium/index.vue
@@ -21,7 +21,7 @@ const mainFeatures = [
 useSeoMeta({
   title: 'Premium',
 
-  description: 'Enhance your experience'
+  description: 'Enhance your experience with additional features'
 })
 
 definePageMeta({
@@ -62,7 +62,8 @@ definePageMeta({
 
             <p class='mx-auto mt-4 lg:mt-3 max-w-2xl text-center text-base leading-8'>
               <!-- TODO: Improve description -->
-              Enhance your experience with additional features
+              Get access to {{ completeBooruList.length - defaultBooruList.length }} additional Boorus,
+              and {{ mainFeatures.length }} exclusive features!
             </p>
           </div>
 
@@ -71,9 +72,29 @@ definePageMeta({
                     href='https://gum.co/Rule34App#Ratings' target='_blank'>
             <div class='flex items-center'>
               <StarIcon v-for='rating in [0, 1, 2, 3]' :key='rating' aria-hidden='true' class='h-5 w-5 flex-shrink-0 text-base-content-highlight' />
-              <StarIcon aria-hidden='true' class='h-5 w-5 flex-shrink-0' />
 
-              <p class='sr-only'>4 out of 5 stars</p>
+              <!-- Fix for half star https://github.com/tailwindlabs/heroicons/discussions/424 -->
+              <svg
+                aria-hidden='true'
+                class='h-5 w-5 flex-shrink-0'
+                fill='none'
+                viewBox='0 0 20 20'
+                xmlns='http://www.w3.org/2000/svg'
+              >
+                <defs>
+                  <linearGradient id='something'>
+                    <stop offset='60%' stop-color='#e5e7eb' />
+                    <stop offset='60%' stop-color='#9ca3af' />
+                  </linearGradient>
+                </defs>
+                <path clip-rule='evenodd'
+                      d='M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401z'
+                      fill='url(#something)'
+                      fill-rule='evenodd'
+                />
+              </svg>
+
+              <p class='sr-only'>4.6 out of 5 stars</p>
             </div>
 
             <span>
@@ -81,6 +102,7 @@ definePageMeta({
 						</span>
           </NuxtLink>
 
+          <!-- TODO: Images of user profiles -->
           <!-- TODO: Testimonials -->
 
           <!-- Body -->
@@ -118,8 +140,7 @@ definePageMeta({
                   <h2 class='text-2xl font-bold tracking-wide text-base-content-highlight'>Premium</h2>
 
                   <p class='mt-2 text-sm leading-7'>
-                    Get access to {{ completeBooruList.length - defaultBooruList.length }} additional Boorus,
-                    and {{ mainFeatures.length }} exclusive features!
+                    Enhance your experience with additional features
                   </p>
                 </div>
 
