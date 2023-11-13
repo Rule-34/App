31f428f6af59afa82cd89c2b62baa02d35ffcf68
Alejandro Akbal <37181533+AlejandroAkbal@users.noreply.github.com>
2023-11-13 11:12:38 +0000

    feat(migrate old data): migrate tag collections correctly and complete page

--- test/feature/pages/premium/migrate-old-data.test.ts
+++ test/feature/pages/premium/migrate-old-data.test.ts
@@ -1,5 +1,8 @@
 import { describe, expect, it } from 'vitest'
 import { createPage, setup } from '@nuxt/test-utils'
+import { migratedTagCollectionsMock, oldLocalStorageVuexUserMock } from './migrate-old-data.mock-data'
+
+oldLocalStorageVuexUserMock
 
 describe('/premium/migrate-old-data', async () => {
   await setup({
@@ -14,13 +17,61 @@ describe('/premium/migrate-old-data', async () => {
     expect(await page.textContent('h1')).toBe('Migrate old data')
   })
 
-  it('migrates old data', async () => {
+  it.only('migrates old data', async () => {
     const page = await createPage('/premium/migrate-old-data')
 
-    // Load data to localStorage
+    // === Arrange
 
-    // Click Button
+    // Load data to localStorage - https://azamuddin.com/en/notes/11032023-playwright-set-localstorage
+    await page.evaluate((localStorageVuexUserMock) => {
+      localStorage.setItem('vuex-user', localStorageVuexUserMock)
+    }, JSON.stringify(oldLocalStorageVuexUserMock))
 
-    // Check data is in IndexedDB & localStorage
+    // await page.reload()
+
+    // === Act
+
+    await page
+      .getByRole('button', { name: /Migrate/i })
+      .click()
+
+    const migratedSettings = await page.evaluate(() => {
+      return {
+        'settings-navigationTouchGestures': localStorage.getItem('settings-navigationTouchGestures'),
+        'settings-postFullSizeImages': localStorage.getItem('settings-postFullSizeImages'),
+        'settings-postsPerPage': localStorage.getItem('settings-postsPerPage')
+      }
+    })
+
+    const migratedTagCollections = await page.evaluate(() => {
+      return JSON.parse(localStorage.getItem('user-tagCollections')!)
+    })
+
+    const migratedBoorus = await page.evaluate(() => {
+      return JSON.parse(localStorage.getItem('user-booruList')!)
+    })
+
+    const isLocalStorageCleaned = await page.evaluate(() => {
+      return localStorage.getItem('vuex-user') === null
+    })
+
+    const currentUrl = new URL(page.url())
+
+    // === Assert
+
+    // Redirected successfully
+    expect(currentUrl.pathname).toBe('/premium')
+
+    expect(migratedSettings).toEqual({
+      'settings-navigationTouchGestures': 'false',
+      'settings-postFullSizeImages': 'true',
+      'settings-postsPerPage': '69'
+    })
+
+    expect(migratedTagCollections).toEqual(migratedTagCollectionsMock)
+
+    expect(migratedBoorus).toEqual()
+
+    expect(isLocalStorageCleaned).toBe(true)
   })
 })
