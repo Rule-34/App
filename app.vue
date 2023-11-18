87b20b77091e70fb41709784f8267633fb664d6a
Alejandro Akbal <37181533+AlejandroAkbal@users.noreply.github.com>
2023-11-18 21:32:24 +0000

    refactor: use useHead

--- app.vue
+++ app.vue
@@ -69,6 +69,27 @@
         type: 'text/css',
         href: 'https://rsms.me/inter/inter.css'
       }
+    ],
+
+    script: [
+      {
+        type: 'text/partytown',
+        children: `
+          var _paq = window._paq = window._paq || [];
+
+          _paq.push(["disableCookies"]);
+          _paq.push(['trackPageView']);
+          _paq.push(['enableLinkTracking']);
+
+          (function() {
+            var u="https://matomo.akbal.dev/";
+            _paq.push(['setTrackerUrl', u+'matomo.php']);
+            _paq.push(['setSiteId', '1']);
+            var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+            g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+          })();
+        `
+      }
     ]
   })
 
@@ -102,12 +123,4 @@
   <NuxtLayout>
     <NuxtPage />
   </NuxtLayout>
-
-  <!-- Matomo -->
-  <Script type="text/partytown">
-    var _paq = window._paq = window._paq || []; _paq.push(["disableCookies"]); _paq.push(['trackPageView']);
-    _paq.push(['enableLinkTracking']); (function() { var u="https://matomo.akbal.dev/"; _paq.push(['setTrackerUrl',
-    u+'matomo.php']); _paq.push(['setSiteId', '1']); var d=document, g=d.createElement('script'),
-    s=d.getElementsByTagName('script')[0]; g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s); })();
-  </Script>
 </template>
