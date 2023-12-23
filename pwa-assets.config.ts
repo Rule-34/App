9b8890b66ed252d03c54953628365b1a54ea990b
Alejandro Akbal <37181533+AlejandroAkbal@users.noreply.github.com>
2023-12-23 15:43:38 +0000

    feat(pwa): fix icon

--- pwa-assets.config.ts
+++ pwa-assets.config.ts
@@ -1,6 +1,31 @@
-import { defineConfig, minimalPreset as preset } from '@vite-pwa/assets-generator/config'
+import { defineConfig, minimal2023Preset as preset } from '@vite-pwa/assets-generator/config'
 
 export default defineConfig({
-  preset,
+  headLinkOptions: {
+    preset: '2023'
+  },
+  preset: {
+    ...preset,
+
+    maskable: {
+      ...preset.maskable,
+
+      resizeOptions: {
+        ...preset.maskable.resizeOptions,
+
+        background: '#000000'
+      }
+    },
+
+    apple: {
+      ...preset.apple,
+
+      resizeOptions: {
+        ...preset.apple.resizeOptions,
+
+        background: '#000000'
+      }
+    }
+  },
   images: ['public/icon.svg']
 })
