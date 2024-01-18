97d2cf1435325e23e96df24e7bc9ffb243a34834
Alejandro Akbal <37181533+AlejandroAkbal@users.noreply.github.com>
2024-01-18 13:22:55 +0000

    feat(reset): clear cookies too

--- pages/settings.vue
+++ pages/settings.vue
@@ -18,15 +18,55 @@
       return
     }
 
-    localStorage.clear()
+    // Cookies
+    clearCookies()
 
+    // LocalStorage
+    clearLocalStorage()
+
+    // IndexedDB
+    await clearIndexedDb()
+
+    location.reload()
+  }
+
+  /**
+   * @see https://stackoverflow.com/a/33366171
+   */
+  function clearCookies() {
+    var cookies = document.cookie.split('; ')
+
+    for (var c = 0; c < cookies.length; c++) {
+      var d = window.location.hostname.split('.')
+
+      while (d.length > 0) {
+        var cookieBase =
+          encodeURIComponent(cookies[c].split(';')[0].split('=')[0]) +
+          '=; expires=Thu, 01-Jan-1970 00:00:01 GMT; domain=' +
+          d.join('.') +
+          ' ;path='
+        var p = location.pathname.split('/')
+        document.cookie = cookieBase + '/'
+        while (p.length > 0) {
+          document.cookie = cookieBase + p.join('/')
+          p.pop()
+        }
+
+        d.shift()
+      }
+    }
+  }
+
+  function clearLocalStorage() {
+    localStorage.clear()
+  }
+
+  async function clearIndexedDb() {
     const indexedDBDatabaseNames = await indexedDB.databases()
 
     for (const { name } of indexedDBDatabaseNames) {
       indexedDB.deleteDatabase(name)
     }
-
-    location.reload()
   }
 </script>
 
