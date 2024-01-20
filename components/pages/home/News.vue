43ac38e62a4d3dbf2367ec87080e455e40d5dda1
Alejandro Akbal <37181533+AlejandroAkbal@users.noreply.github.com>
2024-01-20 20:07:27 +0000

    feat: add news

--- components/pages/home/News.vue
+++ components/pages/home/News.vue
@@ -1,6 +1,21 @@
 <script lang="ts" setup>
   const newsList = [
     {
+      name: 'Annoying popup ads solution',
+      text:
+        'There are ads that open in a new tab, BUT:\n' +
+        'Instead of closing that tab, just PRESS THE BACK BUTTON ON YOUR BROWSER\n' +
+        'And you can continue browsing just where you were before, in the same scroll position and everything\n' +
+        '\n' +
+        'If you want to get rid of ads, you can subscribe to Premium, which includes lots of other nice things',
+      links: [
+        {
+          text: 'Premium',
+          href: '/premium'
+        }
+      ]
+    },
+    {
       name: 'Install the App',
       text: 'Learn how to install the Rule 34 App on your device, in 3 simple steps',
       links: [
@@ -15,11 +30,11 @@
       name: 'Social media Bots',
       text: 'Enjoy the best hentai anywhere and anytime',
       links: [
-        {
-          text: 'Twitter',
-          href: 'https://twitter.com/Rule34_Pokebot',
-          target: '_blank'
-        },
+        // {
+        //   text: 'Twitter',
+        //   href: 'https://twitter.com/Rule34_Pokebot',
+        //   target: '_blank'
+        // },
         {
           text: 'Telegram',
           href: 'https://t.me/Rule_34_Hentai',
@@ -28,6 +43,17 @@
         {
           text: 'Mastodon',
           href: 'https://mastodon.social/@r34',
+          target: '_blank'
+        }
+      ]
+    },
+    {
+      name: 'We are open source',
+      text: 'Check out and contribute to our source code',
+      links: [
+        {
+          text: 'GitHub',
+          href: 'https://redirect.r34.app/github',
           target: '_blank'
         }
       ]
@@ -44,8 +70,8 @@
       <ContentContainer
         :links="news.links"
         :text="news.text"
-        as="h3"
         :title="news.name"
+        as="h3"
       />
     </li>
   </ol>
