2e4659393268bb63c8478cccf9d525a97b532ee1
Alejandro Akbal <37181533+AlejandroAkbal@users.noreply.github.com>
2024-02-16 15:41:25 +0000

    chore: add info to Premium links

--- components/pages/home/News.vue
+++ components/pages/home/News.vue
@@ -1,51 +1,51 @@
 <script lang="ts" setup>
-  const newsList = [
-    {
-      name: 'Farewell, redirecting ads!',
-      text:
-        "To improve your experience, I've removed all 3rd party ads." +
-        '\nHowever, maintaining and developing this project takes resources. As the sole developer, I rely on your support. ' +
-        'Consider subscribing to the Premium version for an ad-free experience and exclusive features',
-      links: [
-        {
-          text: 'Premium',
-          href: '/premium'
-        }
-      ]
-    },
-    {
-      name: 'Install the App',
-      text: 'Learn how to install the Rule 34 App on your device, in 3 simple steps',
-      links: [
-        {
-          text: 'Install the App',
-          href: 'https://www.installpwa.com/from/r34.app',
-          target: '_blank'
-        }
-      ]
-    },
-    {
-      name: 'Social media Bots',
-      text: 'Enjoy the best hentai anywhere and anytime',
-      links: [
-        // {
-        //   text: 'Twitter',
-        //   href: 'https://twitter.com/Rule34_Pokebot',
-        //   target: '_blank'
-        // },
-        {
-          text: 'Telegram',
-          href: 'https://t.me/Rule_34_Hentai',
-          target: '_blank'
-        },
-        {
-          text: 'Mastodon',
-          href: 'https://mastodon.social/@r34',
-          target: '_blank'
-        }
-      ]
-    }
-  ]
+const newsList = [
+  {
+    name: 'Farewell, redirecting ads!',
+    text:
+      'To improve your experience, I\'ve removed all 3rd party ads.' +
+      '\nHowever, maintaining and developing this project takes resources. As the sole developer, I rely on your support. ' +
+      'Consider subscribing to the Premium version for an ad-free experience and exclusive features',
+    links: [
+      {
+        text: 'Premium',
+        href: '/premium?utm_source=web&utm_medium=news'
+      }
+    ]
+  },
+  {
+    name: 'Install the App',
+    text: 'Learn how to install the Rule 34 App on your device, in 3 simple steps',
+    links: [
+      {
+        text: 'Install the App',
+        href: 'https://www.installpwa.com/from/r34.app',
+        target: '_blank'
+      }
+    ]
+  },
+  {
+    name: 'Social media Bots',
+    text: 'Enjoy the best hentai anywhere and anytime',
+    links: [
+      // {
+      //   text: 'Twitter',
+      //   href: 'https://twitter.com/Rule34_Pokebot',
+      //   target: '_blank'
+      // },
+      {
+        text: 'Telegram',
+        href: 'https://t.me/Rule_34_Hentai',
+        target: '_blank'
+      },
+      {
+        text: 'Mastodon',
+        href: 'https://mastodon.social/@r34',
+        target: '_blank'
+      }
+    ]
+  }
+]
 </script>
 
 <template>
--- components/pages/posts/post/PostDownload.vue
+++ components/pages/posts/post/PostDownload.vue
@@ -1,67 +1,67 @@
 <script setup>
-  import { ArrowDownTrayIcon } from '@heroicons/vue/24/outline'
-  import { toast } from 'vue-sonner'
+import {ArrowDownTrayIcon} from '@heroicons/vue/24/outline'
+import {toast} from 'vue-sonner'
 
-  const props = defineProps({
-    mediaName: {
-      type: String,
-      required: true
-    },
+const props = defineProps({
+  mediaName: {
+    type: String,
+    required: true
+  },
 
-    mediaUrl: {
-      type: String,
-      required: true
+  mediaUrl: {
+    type: String,
+    required: true
+  }
+})
+
+const {isPremium} = useUserData()
+
+async function downloadMedia() {
+  if (!isPremium.value) {
+    toast.info('Premium feature', {
+      description: 'Automatically download media with the correct name & extension: rule34.xxx-123.png',
+      action: {
+        label: 'Subscribe',
+        onClick: () => navigateTo('/premium?utm_source=web&utm_medium=download-media')
+      }
+    })
+    return
+  }
+
+  const {proxiedUrl} = useProxyHelper(props.mediaUrl)
+
+  const response = await $fetch.raw(proxiedUrl.value, {
+    responseType: 'blob',
+
+    onResponseError(context) {
+      toast.error(`Failed to download media: ${context.error.message}`)
+      throw context.error
     }
   })
 
-  const { isPremium } = useUserData()
+  const FILE_EXTENSION = response.headers.get('content-type').split('/')[1]
 
-  async function downloadMedia() {
-    if (!isPremium.value) {
-      toast.info('Premium feature', {
-        description: 'Automatically download media with the correct name & extension: rule34.xxx-123.png',
-        action: {
-          label: 'Subscribe',
-          onClick: () => navigateTo('/premium')
-        }
-      })
-      return
-    }
+  const FILE_NAME = props.mediaName + '.' + FILE_EXTENSION
 
-    const { proxiedUrl } = useProxyHelper(props.mediaUrl)
+  downloadBlobToDevice(response._data, FILE_NAME)
+}
 
-    const response = await $fetch.raw(proxiedUrl.value, {
-      responseType: 'blob',
+function downloadBlobToDevice(blob, fileName) {
+  const BLOB_OBJECT_URL = URL.createObjectURL(blob)
 
-      onResponseError(context) {
-        toast.error(`Failed to download media: ${context.error.message}`)
-        throw context.error
-      }
-    })
+  const LINK = document.createElement('a')
 
-    const FILE_EXTENSION = response.headers.get('content-type').split('/')[1]
+  LINK.href = BLOB_OBJECT_URL
+  LINK.target = '_blank'
+  LINK.download = fileName
+  LINK.style.display = 'none'
 
-    const FILE_NAME = props.mediaName + '.' + FILE_EXTENSION
+  document.body.appendChild(LINK)
+  LINK.click()
+  document.body.removeChild(LINK)
 
-    downloadBlobToDevice(response._data, FILE_NAME)
-  }
-
-  function downloadBlobToDevice(blob, fileName) {
-    const BLOB_OBJECT_URL = URL.createObjectURL(blob)
-
-    const LINK = document.createElement('a')
-
-    LINK.href = BLOB_OBJECT_URL
-    LINK.target = '_blank'
-    LINK.download = fileName
-    LINK.style.display = 'none'
-
-    document.body.appendChild(LINK)
-    LINK.click()
-    document.body.removeChild(LINK)
-
-    URL.revokeObjectURL(BLOB_OBJECT_URL)
-  }
+  URL.revokeObjectURL(BLOB_OBJECT_URL)
+}
 </script>
 
 <template>
@@ -72,6 +72,6 @@
   >
     <span class="sr-only"> Download post </span>
 
-    <ArrowDownTrayIcon class="group-hover:hover-text-util h-5 w-5 text-base-content" />
+    <ArrowDownTrayIcon class="group-hover:hover-text-util h-5 w-5 text-base-content"/>
   </button>
 </template>
