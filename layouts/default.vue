5cceabad7cc54e784abc443163a1e660d82a4709
Alejandro Akbal <37181533+AlejandroAkbal@users.noreply.github.com>
2024-03-01 09:13:56 +0000

    chore: remove overflow from bg

--- layouts/default.vue
+++ layouts/default.vue
@@ -36,10 +36,10 @@
     <!-- Background -->
     <div
       aria-hidden="true"
-      class="absolute inset-x-0 -z-10 flex transform-gpu justify-center overflow-hidden blur-xl md:blur-3xl"
+      class="absolute inset-x-0 -z-10 flex transform-gpu justify-center blur-xl md:blur-3xl"
     >
       <div
-        class="-mr-3 -mt-3 aspect-[1108/632] w-full flex-none bg-gradient-to-l from-primary-300 to-accent-700 opacity-25"
+        class="aspect-[1108/632] w-full flex-none bg-gradient-to-l from-primary-300 to-accent-700 opacity-25"
         style="
           clip-path: polygon(
             100% 0%,
