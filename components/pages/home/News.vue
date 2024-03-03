c4ce938dcd86aa80315bc615a48679c7397c1448
Alejandro Akbal <37181533+AlejandroAkbal@users.noreply.github.com>
2024-03-03 22:19:08 +0000

    chore: update news

--- components/pages/home/News.vue
+++ components/pages/home/News.vue
@@ -1,21 +1,27 @@
 <script lang="ts" setup>
   const newsList = [
     {
-      name: 'Farewell, redirecting ads!',
+      name: 'Get the best Hentai on social media! 📱',
       text:
-        "To improve your experience, I've removed all 3rd party ads." +
-        '\nHowever, maintaining and developing this project takes resources. As the sole developer, I rely on your support. ' +
-        'Consider subscribing to the Premium version for an ad-free experience and exclusive features',
+        "We're on Twitter, Mastodon, and Telegram",
       links: [
         {
-          text: 'Premium',
-          href: '/premium?utm_source=internal&utm_medium=news'
-        }
+          text: 'Twitter',
+          href: 'https://twitter.com/Rule34_Pokebot'
+        },
+        {
+          text: 'Mastodon',
+          href: 'https://mastodon.social/@r34'
+        },
+        {
+          text: 'Telegram',
+          href: 'https://t.me/Rule_34_Hentai'
+        },
       ]
     },
     {
       name: 'Install the App',
-      text: 'Learn how to install the Rule 34 App on your device, in 3 simple steps',
+      text: 'Learn how to install the Rule 34 App, in 3 simple steps',
       links: [
         {
           text: 'Install the App',
@@ -26,7 +32,7 @@
     },
     {
       name: 'Blocked in your country? 🇷🇺',
-      text: 'Go to the alternative site, alt.r34.app, to access the Rule 34 App without restrictions',
+      text: 'Access the Rule 34 App without restrictions, with the alternative site',
       links: [
         {
           text: 'alt.r34.app',
