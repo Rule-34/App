96671a38fbd19c2caaa37afbba80236cdbb682af
Alejandro Akbal <37181533+AlejandroAkbal@users.noreply.github.com>
2024-03-11 13:04:37 +0000

    refactor: z indexes

--- components/layout/navigation/SidebarWrapper.vue
+++ components/layout/navigation/SidebarWrapper.vue
@@ -12,7 +12,7 @@
     >
       <HeadlessDialog
         as="div"
-        class="relative z-40"
+        class="relative z-10"
         @close="toggleMenu(false)"
       >
         <!-- Background -->
