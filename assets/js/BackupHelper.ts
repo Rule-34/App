f8bba5c4e1f8e29a28f402b4cae1616ba75b0016
Alejandro Akbal <37181533+AlejandroAkbal@users.noreply.github.com>
2024-03-30 18:42:50 +0000

    chore: remove all dexie code

--- assets/js/BackupHelper.ts
+++ assets/js/BackupHelper.ts
@@ -1,10 +1,7 @@
-import { ClientResponseError } from 'pocketbase'
 import type { Domain } from '~/assets/js/domain'
 import type { IOldBackupState } from '~/assets/js/oldBackup'
 import type { ITagCollection } from '~/assets/js/tagCollection.dto'
 import { booruTypeList } from '~/assets/lib/rule-34-shared-resources/src/util/BooruUtils'
-import { db } from '~/store/SavedPosts'
-import { PocketbasePost } from './pocketbase.dto'
 
 export interface IBackupState {
   version: number
@@ -109,43 +106,4 @@ export async function tryToRestoreV2OrV3Backup(backup: any): Promise<void> {
   // TODO: Verify object with Zod
 
   await restoreV3Backup(backup)
-}
-
-export async function doesBrowserHaveSavedPosts(): Promise<boolean> {
-  const savedPosts = await db.posts.count()
-
-  if (savedPosts > 0) {
-    return true
-  }
-
-  return false
-}
-
-export async function migrateSavedPostsToCloud() {
-  const { $pocketBase } = useNuxtApp()
-
-  const savedPosts = await db.posts.toArray()
-
-  if (!savedPosts.length) {
-    return
-  }
-
-  for (const post of savedPosts) {
-
-    try {
-
-      post.data.domain = post.original_domain
-
-      await $pocketBase.collection('posts').create(PocketbasePost.fromPost(post.data, $pocketBase.authStore.baseModel.id))
-
-    } catch (error) {
-
-      // Skip if post already exists
-      if (error instanceof ClientResponseError && 'original_domain' in error.data && error.data.original_domain.code === 'validation_not_unique') {
-        continue
-      }
-    }
-  }
-
-  await db.delete()
 }
