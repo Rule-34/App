38d98ae0439336442cbb354a5c2af3eb2371bbdb
Alejandro Akbal <37181533+AlejandroAkbal@users.noreply.github.com>
2024-04-01 21:25:37 +0100

    chore: restore nuxt schema

--- app.vue
+++ app.vue
@@ -54,23 +54,23 @@ useSeoMeta({
   })
 })
 
-// useSchemaOrg([
-//   defineWebSite({
-//     name: 'Rule 34 App'
-//   }),
-//   defineWebPage(),
-//   defineOrganization({
-//     name: 'Rule 34 App',
-//     logo: '/icon.svg',
-//     sameAs: [
-//       'https://rule34.app',
-//       'https://twitter.com/Rule34App',
-//       'https://twitter.com/Rule34App',
-//       'https://discord.gg/fUhYHSZ',
-//       'https://github.com/Rule-34/App'
-//     ]
-//   })
-// ])
+  useSchemaOrg([
+    defineWebSite({
+      name: 'Rule 34 App'
+    }),
+    defineWebPage(),
+    defineOrganization({
+      name: 'Rule 34 App',
+      logo: '/icon.svg',
+      sameAs: [
+        'https://rule34.app',
+        'https://twitter.com/Rule34App',
+        'https://twitter.com/Rule34App',
+        'https://discord.gg/fUhYHSZ',
+        'https://github.com/Rule-34/App'
+      ]
+    })
+  ])
 </script>
 
 <template>
