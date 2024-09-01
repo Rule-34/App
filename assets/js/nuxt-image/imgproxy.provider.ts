3f7cb6a259748b6831232e5d0b76fcf466df2663
Alejandro Akbal <37181533+AlejandroAkbal@users.noreply.github.com>
2024-09-01 20:41:39 +0100

    fix: changes from deps

--- assets/js/nuxt-image/imgproxy.provider.ts
+++ assets/js/nuxt-image/imgproxy.provider.ts
@@ -1,48 +1,44 @@
-import { joinURL } from "ufo";
-import { createOperationsGenerator } from "@nuxt/image/dist/runtime/utils/index";
-import type { ProviderGetImage } from "@nuxt/image";
-import { Buffer } from "buffer";
+import { joinURL } from 'ufo'
+import type { ProviderGetImage } from '@nuxt/image'
+import { Buffer } from 'buffer'
+import { createOperationsGenerator } from '#image/utils'
 
 // https://docs.imgproxy.net/
 const operationsGenerator = createOperationsGenerator({
   keyMap: {
-    resize: "rs",
-    size: "s",
-    fit: "rt",
-    width: "w",
-    height: "h",
-    dpr: "dpr",
-    enlarge: "el",
-    extend: "ex",
-    gravity: "g",
-    crop: "c",
-    padding: "pd",
-    trim: "t",
-    rotate: "rot",
-    quality: "q",
-    maxBytes: "mb",
-    background: "bg",
-    backgroundAlpha: "bga",
-    blur: "bl",
-    sharpen: "sh",
-    watermark: "wm",
-    preset: "pr",
-    cacheBuster: "cb",
-    stripMetadata: "sm",
-    stripColorProfile: "scp",
-    autoRotate: "ar",
-    filename: "fn",
-    format: "f",
+    resize: 'rs',
+    size: 's',
+    fit: 'rt',
+    width: 'w',
+    height: 'h',
+    dpr: 'dpr',
+    enlarge: 'el',
+    extend: 'ex',
+    gravity: 'g',
+    crop: 'c',
+    padding: 'pd',
+    trim: 't',
+    rotate: 'rot',
+    quality: 'q',
+    maxBytes: 'mb',
+    background: 'bg',
+    backgroundAlpha: 'bga',
+    blur: 'bl',
+    sharpen: 'sh',
+    watermark: 'wm',
+    preset: 'pr',
+    cacheBuster: 'cb',
+    stripMetadata: 'sm',
+    stripColorProfile: 'scp',
+    autoRotate: 'ar',
+    filename: 'fn',
+    format: 'f'
   },
-  formatter: (key, value) => `${key}:${value}`,
-});
+  formatter: (key, value) => `${key}:${value}`
+})
 
 function urlSafeBase64(string: string) {
-  return Buffer.from(string, "utf8")
-    .toString("base64")
-    .replace(/=/g, "")
-    .replace(/\+/g, "-")
-    .replace(/\//g, "_");
+  return Buffer.from(string, 'utf8').toString('base64').replace(/=/g, '').replace(/\+/g, '-').replace(/\//g, '_')
 }
 
 const defaultModifiers = {
@@ -52,33 +48,32 @@ const defaultModifiers = {
   // gravity: "no",
   // enlarge: 1,
   // format: "webp",
-};
+}
 
 /**
- * 
+ *
  * @see https://github.com/nuxt/image/issues/378
  */
 export const getImage: ProviderGetImage = (src, options) => {
-
   // Skip if src is a relative URL
-  if (src.startsWith("/")) {
-    return { url: src };
+  if (src.startsWith('/')) {
+    return { url: src }
   }
 
   // Skip GIFs, since imgproxy doesn't support them
-  if (src.endsWith(".gif")) {
-    return { url: src };
+  if (src.endsWith('.gif')) {
+    return { url: src }
   }
 
-  const { modifiers, baseURL } = options;
+  const { modifiers, baseURL } = options
 
-  const mergeModifiers = { ...defaultModifiers, ...modifiers };
+  const mergeModifiers = { ...defaultModifiers, ...modifiers }
 
-  const encodedUrl = urlSafeBase64(src);
+  const encodedUrl = urlSafeBase64(src)
 
-  const path = joinURL("/", operationsGenerator(mergeModifiers), encodedUrl);
+  const path = joinURL('/', operationsGenerator(mergeModifiers), encodedUrl)
 
   return {
-    url: joinURL(baseURL, path),
-  };
-};
+    url: joinURL(baseURL, path)
+  }
+}
