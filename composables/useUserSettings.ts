0a2e28dbec1a63a23e10bcb06c47741d68e254d3
Alejandro Akbal <37181533+AlejandroAkbal@users.noreply.github.com>
2024-09-11 13:21:18 +0100

    fix: mismatch values when SSR

--- composables/useUserSettings.ts
+++ composables/useUserSettings.ts
@@ -1,7 +1,7 @@
 import { useStorage } from '@vueuse/core'
 
 let postFullSizeImages = ref<boolean>(false)
-let postsPerPage = ref<number>(30)
+let postsPerPage = ref<number>(29)
 
 if (process.client) {
   postFullSizeImages = useStorage('settings-postFullSizeImages', false, localStorage, {
