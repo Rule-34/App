23d5e7aa01916bf46b4c82f2984ad130a69db3c3
Alejandro Akbal <37181533+AlejandroAkbal@users.noreply.github.com>
2024-11-27 16:35:08 +0000

    fix: use correct url

--- components/layout/FeedbackButton.vue
+++ components/layout/FeedbackButton.vue
@@ -7,10 +7,10 @@
     <div class="flex flex-col items-center justify-center gap-2">
       <NuxtLink
         class="focus-visible:focus-outline-util hover:hover-text-util hover:hover-bg-util flex items-center justify-center gap-2 rounded-r-md bg-base-1000/60 px-1 py-2 shadow-lg ring-1 ring-base-0/20 backdrop-blur"
+        rel="nofollow noopener noreferrer"
         style="writing-mode: vertical-lr; transform: rotate(180deg)"
         target="_blank"
-        to="https://forms.gle/oHukbtLHdot6CuB98"
-        rel='nofollow noopener noreferrer'
+        to="https://feedback.r34.app"
       >
         <span class="text-sm"> Feedback </span>
 
