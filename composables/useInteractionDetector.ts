aa84e22b8422ebe0e5b684dc6f85ff5eaac5c10f
Alejandro Akbal <37181533+AlejandroAkbal@users.noreply.github.com>
2024-12-18 19:22:07 +0000

    feat(interaction detection): ignore interaction with dialogs

--- composables/useInteractionDetector.ts
+++ composables/useInteractionDetector.ts
@@ -8,10 +8,27 @@ export function useInteractionDetector() {
   }
 
   const interactionEvents = ['mousedown', 'touchstart']
+  const ignoreSelectors = [
+    // Cookie Consent
+    '#cc-main',
+    // Dialogs
+    '[role="dialog"]'
+  ]
 
   const cleanup = interactionEvents.map((event) => useEventListener(window, event, handleInteraction))
 
-  function handleInteraction() {
+  function handleInteraction(event: Event) {
+    //
+
+    // Ignore interactions on elements matching certain selectors
+    if (event.target instanceof Element) {
+      const target = event.target
+
+      if (ignoreSelectors.some((selector) => target.closest(selector))) {
+        return
+      }
+    }
+
     hasInteracted.value = true
     cleanup.forEach((unregister) => unregister())
   }
