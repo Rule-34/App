c6ba815081a4ebb4203541e9c8d24823ef92a3f5
Alejandro Akbal <37181533+AlejandroAkbal@users.noreply.github.com>
2025-01-03 12:26:08 +0000

    chore: change ads

--- composables/useAdvertisements.ts
+++ composables/useAdvertisements.ts
@@ -4,16 +4,17 @@ export default function () {
   const adScript = useState<string>('adScript', () => '')
 
   const weightedAds = [
-    // Adsession
-    // {
-    //   id: '/js/popunder.js?v=5',
-    //   weight: 1
-    // },
-    // Hilltopads
+    // Adsession - Popunder
     {
-      id: 'https://messyadvance.com/ckDt9/6Fb.2/5/ltSWWDQe9VNRThUn2/M/D/AH4/MFCG0j1-NrT/YHw/MBDWgHx-',
+      id: '/js/popunder.js?v=7',
       weight: 1
-    }
+    },
+
+    // Hilltopads - Popunder
+    // {
+    //   id: 'https://messyadvance.com/ckDt9/6Fb.2/5/ltSWWDQe9VNRThUn2/M/D/AH4/MFCG0j1-NrT/YHw/MBDWgHx-',
+    //   weight: 1
+    // }
   ]
 
   if (!adScript.value) {
@@ -29,9 +30,17 @@ export default function () {
     'data-cfasync': 'false'
   })
 
-  // PartnersHouse
+  // PartnersHouse - InPage Push
+  // useScript({
+  //   src: 'https://news-bbipasu.today/process.js?id=1278157271',
+  //   async: false,
+  //   defer: true,
+  //   'data-cfasync': 'false'
+  // })
+
+  // EvaDav - InPage Push
   useScript({
-    src: 'https://news-bbipasu.today/process.js?id=1278157271',
+    src: 'https://udzpel.com/pw/waWQiOjExOTMwMzUsInNpZCI6MTQwNzY1NSwid2lkIjo2ODMzODcsInNyYyI6Mn0=eyJ.js',
     async: false,
     defer: true,
     'data-cfasync': 'false'
