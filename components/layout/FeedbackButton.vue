ddcbf77dde01cc2076c4b6c5a6e7415c6948249a
Alejandro Akbal <37181533+AlejandroAkbal@users.noreply.github.com>
2025-02-13 17:01:02 +0000

    feat(feedback button): make more prominent

--- components/layout/FeedbackButton.vue
+++ components/layout/FeedbackButton.vue
@@ -6,16 +6,16 @@
   <div class="fixed bottom-1/2 right-0">
     <div class="flex flex-col items-center justify-center gap-2">
       <NuxtLink
-        class="focus-visible:focus-outline-util hover:hover-text-util hover:hover-bg-util flex items-center justify-center gap-2 rounded-r-md bg-base-1000/60 px-1 py-2 shadow-lg ring-1 ring-base-0/20 backdrop-blur"
+        class="focus-visible:focus-outline-util hover:hover-text-util hover:hover-bg-util flex items-center justify-center gap-2 rounded-r-md bg-base-1000 px-1 py-2 ring-1 ring-base-0/15"
         rel="nofollow noopener noreferrer"
         style="writing-mode: vertical-lr; transform: rotate(180deg)"
         target="_blank"
         to="https://feedback.r34.app"
       >
-        <span class="text-sm"> Feedback </span>
+        <span class="text-base font-medium"> Feedback </span>
 
         <PencilSquareIcon
-          class="h-4 w-4"
+          class="h-5 w-5"
           style="transform: rotate(180deg)"
         />
       </NuxtLink>
