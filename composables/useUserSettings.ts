3fa4273cda022afd63e20212081a835777c553a3
Alejandro Akbal <37181533+AlejandroAkbal@users.noreply.github.com>
2026-01-26 00:23:03 +0700

    Revert "fix: temporarily enable full size images for rule34.xxx CDN errors"
    This reverts commit 5f11bb3bd21f327dc41735c418a4e8925f89aa21.

--- composables/useUserSettings.ts
+++ composables/useUserSettings.ts
@@ -1,13 +1,13 @@
 import { useLocalStorage } from '@vueuse/core'
 
 export default function () {
-  let postFullSizeImages = ref<boolean>(true)
+  let postFullSizeImages = ref<boolean>(false)
   let postsPerPage = ref<number>(29)
   let autoplayAnimatedMedia = ref<boolean>(false)
   let blockAiGeneratedImages = ref<boolean>(false)
 
   if (import.meta.client) {
-    postFullSizeImages = useLocalStorage('settings-postFullSizeImages', true, {
+    postFullSizeImages = useLocalStorage('settings-postFullSizeImages', false, {
       writeDefaults: false
     })
     postsPerPage = useLocalStorage('settings-postsPerPage', 29, {
