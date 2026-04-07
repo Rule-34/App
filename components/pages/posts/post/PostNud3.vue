3b602e7b1abe09d3d962404b62a3f640979b2984
vaguue <little_scamp@yahoo.com>
2026-04-07 18:43:53 +0300

    new button look

--- components/pages/posts/post/PostNud3.vue
+++ components/pages/posts/post/PostNud3.vue
@@ -11,13 +11,14 @@
 
 <template>
   <a
-    aria-label="Undress photo"
-    title="Undress photo"
-    class="hover:hover-bg-util focus-visible:focus-outline-util group rounded-md px-1.5 py-1"
+    aria-label="Generate AI Video"
+    title="Generate AI Video"
+    class="hover:hover-bg-util focus-visible:focus-outline-util group flex items-center gap-1 rounded-md px-1.5 py-1"
     :href="`https://nud3.me/pornify?imageUrl=${encodeURIComponent(mediaUrl)}&r=r34`"
     target="_blank"
     rel="noopener noreferrer"
   >
-    <SparklesIcon aria-hidden="true" class="group-hover:hover-text-util text-base-content h-5 w-5" />
+    <SparklesIcon aria-hidden="true" class="h-5 w-5 text-[#F0489C] group-hover:text-[#BD2671]" />
+    <span class="text-xs font-bold text-[#F0489C] group-hover:text-[#BD2671]">AI Video</span>
   </a>
 </template>
