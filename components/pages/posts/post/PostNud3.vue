502f3931ab4b5b007533d222c3ce51bd6e3a0fa1
vaguue <little_scamp@yahoo.com>
2026-04-19 17:11:04 +0300

    remove colors

---
+++ components/pages/posts/post/PostNud3.vue
@@ -0,0 +1,27 @@
+<script lang="ts" setup>
+  import { SparklesIcon } from '@heroicons/vue/24/outline'
+
+  const props = defineProps({
+    mediaUrl: {
+      type: String,
+      required: true
+    }
+  })
+</script>
+
+<template>
+  <a
+    aria-label="Generate AI Video"
+    title="Generate AI Video"
+    class="hover:hover-bg-util focus-visible:focus-outline-util group flex items-center gap-1 rounded-md px-1.5 py-1"
+    :href="`https://nud3.me/pornify?imageUrl=${encodeURIComponent(mediaUrl)}&r=r34`"
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    <SparklesIcon aria-hidden="true" class="group-hover:hover-text-util text-base-content h-5 w-5" />
+    <span class="group-hover:hover-text-util text-base-content text-xs font-bold">
+      <span class="sm:hidden">AI</span>
+      <span class="hidden sm:inline">AI Video</span>
+    </span>
+  </a>
+</template>
